b485a76bbd012af81ac2b798de9cbb9d0f39bfa6
Margaret Lazarovits <mlazarovits@gmail.com>
2023-06-13 15:11:26 -0500

    added Dirichlet + Wishart distribution constant calculations

--- include/BasePDFMixture.hh
+++ include/BasePDFMixture.hh
@@ -41,46 +41,17 @@ class BasePDFMixture{
 		}
 		
 		//gaussian for one data point
-		double Gaus(const Point& x, const Matrix& mu, const Matrix& cov){
-			double det = cov.det();
-			int dim = x.Dim();
-			if(dim != mu.GetDims()[0]){
-				cout << "Error: x and mu length do not match. " << dim << " " << mu.GetDims()[0] << endl;
-				return 0;
-			}
-			if(mu.GetDims()[0] != cov.GetDims()[0]){
-				cout << "Error: covariance and mu dimensions do not match. " << cov.GetDims()[0] << " " << mu.GetDims()[0] << endl;
-				return 0;
-			}
-			if(!cov.square()){
-				cout << "Error: non-square covariance matrix." << endl;
-				return 0;
-			}
-			//construct x - mu
-			Matrix x_mat = Matrix(x.Value());
-			Matrix x_min_mu;
-			x_min_mu.mult(mu,-1.);
-			x_min_mu.add(x_mat);
-			
-			//transpose x - mu
-			Matrix x_min_muT;
-			x_min_muT.transpose(x_min_mu);
-			Matrix cov_inv;
-			cov_inv.invert(cov);
-			
-			double coeff = 1/(pow(det,0.5)*pow(2*acos(-1),0.5*dim));
-			//should only be 1 element matrix
-			//muT*cov*mu = 1xd * dxd * dx1
-			Matrix mat_expon = Matrix(1,1);
-			Matrix cov_mu = Matrix(m_dim,1);
-			
-			cov_mu.mult(cov_inv,x_min_mu);
-			mat_expon.mult(x_min_muT,cov_mu);
-			double expon = mat_expon.at(0,0);
-			return coeff*exp(-0.5*expon);
-			
-		}
+		double Gaus(const Point& x, const Matrix& mu, const Matrix& cov);
 
+
+		//Dirichlet coefficient (eq. B.23)
+		double Dir_C(vector<double> alphas);
+
+		//Wishart entropy normalization (eq. B.79)
+		double Wish_B(Matrix W, double nu);
+
+		//Wishart entropy (eq. B.82)
+		double Wish_H(Matrix W, double nu);
 
 		//data to fit
 		PointCollection m_x;
