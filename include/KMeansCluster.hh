f5940b86f54ddbdb788c864d446d7d8bd0cbf47d
Margaret Lazarovits <mlazarovits@gmail.com>
2023-07-24 12:00:47 -0500

    replaced vector m_assigns with Matrix m_post

--- include/KMeansCluster.hh
+++ include/KMeansCluster.hh
@@ -23,15 +23,15 @@ class KMeansCluster : public BaseCluster{
 		double EvalLogL(){ return double(m_nchg); }
 
 		void GetMeans(vector<Matrix>& m){ m.clear(); m = m_means; }
-		void GetAssignments(vector<int>& a){ a.clear(); a = m_assigns; }	
-		int GetAssignment(int n){ if(n < m_data->GetNPoints()) return m_assigns[n]; else return -999.; }
+		void GetAssignments(vector<int>& a){ a.clear(); for(int i = 0; i < m_n; i++) a.push_back(m_post.at(i,0)); }	
+		int GetAssignment(int n){ if(n < m_data->GetNPoints()) return m_post.at(n,0); else return -999.; }
 	
 
 	private:
 		//parameters - means (1 per cluster)
 		vector<Matrix> m_means;
 		//assignment values
-		vector<int> m_assigns;
+//		vector<int> m_assigns;
 		//number of points assigned to each cluster
 		vector<int> m_counts;
 		//number of points change assignment
--- src/KMeansCluster.cc
+++ src/KMeansCluster.cc
@@ -13,8 +13,10 @@ void KMeansCluster::Initialize(unsigned long long seed){
 	for(int k = 0; k < m_k; k++){
 		m_means.push_back(Matrix(initpts.at(k)));
 	}
-	
-	for(int n = 0; n < m_n; n++) m_assigns.push_back(0);
+
+	//only keeps track of which cluster assigned to (only 1 col - not multiple for multiple clusters)
+	m_post = Matrix(m_n, 1);	
+	m_post.InitEmpty();
 	for(int k = 0; k < m_k; k++) m_counts.push_back(0);
 }
 
@@ -34,9 +36,9 @@ void KMeansCluster::Estimate(){
 			if(dist < dmin){ dmin = dist; kmin = k; }
 		}
 		//track number of points that change assignments
-		if(kmin != m_assigns[n]) m_nchg++;
+		if(kmin != m_post.at(n,0)) m_nchg++;
 		//update assignment
-		m_assigns[n] = kmin;
+		m_post.SetEntry(kmin,n,0);
 		//keep track of number of points assigned to cluster kmin
 		m_counts[kmin]++;
 	}
@@ -51,8 +53,8 @@ void KMeansCluster::Update(){
 	//sum over data points in cluster they've been assigned to
 	for(int n = 0; n < m_n; n++){
 		for(int d = 0; d < m_dim; d++){
-			val = m_means[m_assigns[n]].at(d,0);
-			m_means[m_assigns[n]].SetEntry(val+m_data->at(n).Value(d),d,0);
+			val = m_means[m_post.at(n,0)].at(d,0);
+			m_means[m_post.at(n,0)].SetEntry(val+m_data->at(n).Value(d),d,0);
 		}
 	}
 	//normalize to get mean for each cluster
