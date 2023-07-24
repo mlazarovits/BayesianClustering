6a22911797a5afe26e42bbaeabcfb4031f66783c
Margaret Lazarovits <mlazarovits@gmail.com>
2023-07-24 15:00:47 -0500

    debug

--- src/BasePDFMixture.cc
+++ src/BasePDFMixture.cc
@@ -1,8 +1,8 @@
 #include "BasePDFMixture.hh"
 
-double Prob(const Point& x){
+double BasePDFMixture::Prob(const Point& x){
 	double ret;
-	for(int i = 0; i < k; i++)
+	for(int i = 0; i < m_k; i++)
 		ret += m_weights[i]*m_model[i]->Prob(x);
 	return ret;
 }
