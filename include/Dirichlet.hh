d9373c479cfcb9772bfe13ef6394c479453d6d82
Margaret Lazarovits <mlazarovits@gmail.com>
2023-07-24 10:30:33 -0500

    added Dirichlet PDF

---
+++ include/Dirichlet.hh
@@ -0,0 +1,29 @@
+#ifndef Dirichlet_HH
+#define Dirichlet_HH
+
+#include "BasePDF.hh"
+#include "Matrix.hh"
+
+class Dirichlet : public BasePDF{
+	public:
+		Dirichlet();
+		Dirichlet(vector<double> alphas){ m_alphas = alphas; m_dim = (int)m_alphas.size(); }
+		virtual ~Dirichlet(){ };
+
+
+		void SetParameters(vector<double> alphas){ m_alphas = alphas; m_dim = (int)m_alphas.size();}
+		void GetParameters(vector<double>& alphas){ alphas.clear(); alphas = m_alphas; }	
+		
+		double Prob(const Point& x);
+
+		double lnC();
+
+	private:
+		int m_dim;
+		//parameters
+		vector<double> m_alphas;
+
+	
+
+};
+#endif
---
+++ src/Dirichlet.cc
@@ -0,0 +1,57 @@
+#include <boost/math/special_functions/gamma.hpp>
+#include "Dirichlet.hh"
+
+using boost::math::tgamma;
+using boost::math::lgamma;
+
+double Dirichlet::Prob(const Point& x){
+	//x needs to be k-dimensional
+	if(x.Dim() != m_dim){
+		cout << "Error: x dimensions"  << x.Dim() << " and PDF dimension " << (int)m_alphas.size() << " incompatible." << endl;
+		return -1;
+	}
+
+	//make sure sum over x dim is one
+	double norm = 0;
+	for(int i = 0; i < m_dim; i++){
+		norm += x.at(i);
+		if(m_alphas[i] < 0){
+			cout << "Error: alpha #" << i << " is less than zero: " << m_alphas[0] << endl;
+			return -1;
+		}
+	}
+	if(norm != 1){
+		cout << "Error: x is not on standard K - 1 simplex (sum over k != 1): " << norm << endl;
+		return -1;
+	}
+
+	double num = 1.;
+	double denom = 0.;
+	double pdf = 1.;
+	for(int k = 0; k < m_dim; k++){
+		num *= tgamma(m_alphas[k]);
+		denom += m_alphas[k];
+		pdf *= pow(x.at(k), m_alphas[k] - 1);
+	}
+	denom = tgamma(denom);
+	//normalizing constant for PDF now
+	norm = 0;
+	norm = num/denom;
+
+	return pdf/norm;
+
+}
+
+
+
+double Dirichlet::lnC(){
+	double alpha_hat = 0;
+	double norm = 0;
+	
+	for(int i = 0; i < m_dim; i++){
+		alpha_hat += m_alphas[i];
+		norm += lgamma(m_alphas[i]);
+	}
+	return lgamma(alpha_hat) - norm;
+
+}
