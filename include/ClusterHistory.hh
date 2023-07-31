d4f154dd69d0ba2c31de0c8e27b9bdf05afe77c0
Margaret Lazarovits <mlazarovits@gmail.com>
2023-07-31 09:26:28 -0500

    moved ClusterTree to ClusterHistory

--- include/ClusterHistory.hh
+++ include/ClusterHistory.hh
@@ -1,17 +1,25 @@
-#ifndef JETTREE_HH
-#define JETTREE_HH
+#ifndef ClusterHistory_HH
+#define ClusterHistory_HH
 
+#include "SearchTree.hh"
 #include "Jet.hh"
+#include <vector>
+
 
 using std::vector;
+using node = SearchTree::node;
 
 //this class keeps track of the cluster (merge) history
-class ClusterTree{
+class ClusterHistory{
 	public:
-		ClusterTree();
-		virtual ~ClusterTree();
+		ClusterHistory();
+		virtual ~ClusterHistory();
 
-		void AddLayer(vector<PointCollection> pcs);	
+
+		//init tree stack with leaf nodes (connected to null, external termination nodes)
+		void Init();
+
+		void AddLayer(vector<PointCollection> pcs, double rk);	
 	
 		//clusters at depth i
 		vector<PointCollection> at(int i);
@@ -24,19 +32,19 @@ class ClusterTree{
 
 		int GetNLayers();
 		int GetNClusters();
+		int GetNClusters(int layer);
 
-		//removal functions?
 
+		void clear(){
+			m_tree.clear();
+			m_rks.clear();
+		}
 
 	private:
 		//m_tree[i] = vector<cluster> clusters at depth i -> m_tree[i][j] = cluster j at depth i
 		vector<vector<PointCollection>> m_tree;
 		//posterior values, index matched to point collections
 		vector<double> m_rks;
-
-
-
-
 
 
 
---
+++ src/ClusterHistory.cc
@@ -0,0 +1,52 @@
+#include "ClusterHistory.hh"
+#include <stdexcept>
+
+using std::logic_error;
+
+ClusterHistory::ClusterHistory(){ };
+
+ClusterHistory::~ClusterHistory(){
+	m_tree.clear();
+	m_rks.clear();
+}
+
+
+
+
+void ClusterHistory::AddLayer(vector<PointCollection> pcs, double rk){
+	m_tree.push_back(pcs);
+	m_rks.push_back(rk);
+}
+
+vector<PointCollection> ClusterHistory::at(int i){
+	return m_tree[i];
+}
+
+PointCollection ClusterHistory::at(int i, int j){
+	return m_tree[i][j];
+}
+
+//return clusters at depth given by posterior value
+vector<PointCollection> ClusterHistory::at(double rk){
+	auto it = std::find(m_rks.begin(), m_rks.end(), rk);
+	
+	return m_tree[it - m_rks.begin()];
+}
+
+
+
+int ClusterHistory::GetNLayers(){
+	return (int)m_tree.size();
+}
+
+int ClusterHistory::GetNClusters(int layer){
+	return (int)m_tree[layer].size();
+}
+int ClusterHistory::GetNClusters(){
+	int cnt = 0;
+	for(int i = 0; i < (int)m_tree.size(); i++)
+		cnt += (int)m_tree[i].size();
+	return cnt;
+}
+
+
