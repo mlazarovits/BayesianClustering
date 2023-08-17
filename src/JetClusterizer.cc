6a3c91e7d608fbaf5a9eb0bb9f08648d4afa82c7
Margaret Lazarovits <mlazarovits@gmail.com>
2023-08-17 16:23:16 -0500

    added function for full clustering algo

--- src/JetClusterizer.cc
+++ src/JetClusterizer.cc
@@ -1,5 +1,7 @@
 #include "JetClusterizer.hh"
 #include "VarClusterViz3D.hh"
+#include "BayesHierCluster.hh"
+#include "FullViz3D.hh"
 #include <TSystem.h>
 
 JetClusterizer::JetClusterizer(){
@@ -15,34 +17,42 @@ JetClusterizer::JetClusterizer(vector<Jet> jets){
 
 JetClusterizer::~JetClusterizer(){ }
 
-void JetClusterizer::Cluster(){
-	/*
-	if(m_nJets > 1){
-		//BHC algo
-		m_newJets = MergeJets(m_oldJets);
-		vector<Jet> subJets;
-		int p1, p2;
-		for(int j = 0; j < (int)m_newJets.size(); j++){
-			subJets = FindSubjets(m_newJets[j]);
-			//set parents for merge (newJet) j
-			BHC.GetSubTrees(p1, p2, j);
-			m_newJets[j].SetParents(m_oldJets[p1],m_oldJets[p2]);
-		}	
-		m_nJets = (int)newJets.size();
-		//TODO: make sure oldJets have children set as newJets in tree
-		m_tree.AddLayer(m_newJets);
-		m_oldJets.clear();
-		m_oldJets = m_newJets;
-		m_newJets.clear();
-		subJets.clear();
-		Cluster();
+void JetClusterizer::Cluster(Jet jet){
+	PointCollection* points = new PointCollection();
+	jet.GetEtaPhiConstituents(*points);
+	double alpha = 0.1;
+	double thresh = 1.;
+	bool viz = false;
+	//Bayesian Hierarchical Clustering algo
+	BayesHierCluster* bhc = new BayesHierCluster(alpha);
+	bhc->SetThresh(thresh);
+	bhc->AddData(points);
+	//each node is a jet - a mixture of gaussians (subjets)
+	vector<node*> tree = bhc->Cluster();
+	if(viz){
+		string fname = "plots/test";
+		FullViz3D plots = FullViz3D(tree);
+		plots.Write(fname);
 	}
-	else{
-		return;
+	//get parameters from subjets - GMM components
+	if(viz) cout << tree.size() << " jets found." << endl;
+	for(int n = 0; n < (int)tree.size(); n++){
+		BasePDFMixture* model = tree[n]->model;
+		if(viz){
+			cout << model->GetNClusters() << " subjets found in jet " << n << endl;
+			cout << "Estimated subjet parameters" << endl;
+			map<string, Matrix> params;
+			for(int i = 0; i < model->GetNClusters(); i++){
+				params = model->GetPriorParameters(i);	
+				cout << "weight " << i << ": " << params["pi"].at(0,0) << endl;
+				cout << "mean " << i << endl;
+				params["mean"].Print();
+				cout << "cov " << i << endl;
+				params["cov"].Print();
+				params.clear();
+			}
+		}
 	}
-
-
-*/
 
 }
 
@@ -138,19 +148,21 @@ GaussianMixture* JetClusterizer::FindSubjets(PointCollection* points, double thr
 	return gmm;
 }
 
+
+
 //crack open Jet and get underlying points
 vector<Jet> JetClusterizer::FindSubjets_etaPhi(Jet jet, double thresh, int maxNit, int maxK, bool viz, double a){
+	vector<Jet> subjets;
+	//vector<JetPoint> rhs;
+	//jet.GetConstituents(rhs);
+	//Point vtx = jet.GetVertex();
 
 	//initialize vector of subjets
-	vector<Jet> subjets;
-	vector<JetPoint> rhs;
-	Point vtx = jet.GetVertex();
+	PointCollection* points = new PointCollection();
+	jet.GetEtaPhiConstituents(*points);
+	int n_pts = points->GetNPoints();
 
-	PointCollection points;
-	jet.GetEtaPhiEConstituents(points);
-	jet.GetConstituents(rhs);
-	int n_pts = points.GetNPoints();
-	
+	/*	
 	//find maxK points with biggest energy - 4th dim
 	points.Sort(3);
 	PointCollection seeds;
@@ -167,89 +179,46 @@ vector<Jet> JetClusterizer::FindSubjets_etaPhi(Jet jet, double thresh, int maxNi
 		pt.clear();
 	}
 	points.Clear();
-	
-	GaussianMixture* gmm = FindSubjets(&newpts, thresh, maxNit, maxK, viz, a);
-	//GaussianMixture* gmm = FindSubjets(&newpts, LogLthresh, maxNit, maxK, viz, &seeds);
+	*/
+	GaussianMixture* gmm = FindSubjets(points, thresh, maxNit, maxK, viz, a);
 	int nsubjets = gmm->GetNClusters();
 
 
-	//TODO: consider edge case where r_nk = r_nk' for all k == k' (all k entries for a point are equal)
-	//assign points to found subjets (clusters)
-	for(int k = 0; k < nsubjets; k++){
-		subjets.push_back(Jet());
-		subjets[k].SetVertex(vtx);
-	}
-	int k_assign;
-	for(int n = 0; n < n_pts; n++){
-		//find k where post_nk is max for point n:
-		k_assign = gmm->GetMaxPointAssignment(n);
-		//assign point n to subjet k - subjets[postMax_k] += rh[n];
-		subjets[k_assign].add(rhs[n]);
-	}
-	
 	return subjets;
 
 }
 
 //crack open Jet and get underlying points
 vector<Jet> JetClusterizer::FindSubjets_XYZ(Jet jet, double thresh, int maxNit, int maxK, bool viz, double a){
+	vector<Jet> subjets;
 
 	//initialize vector of subjets
-	vector<Jet> subjets;
-	vector<JetPoint> rhs;
-/*
+	PointCollection* points = new PointCollection();
+	jet.GetXYZConstituents(*points);
+	int n_pts = points->GetNPoints();
 
-	PointCollection points;
-	VarGaussianMixture vgmm(maxK);
-	jet.GetPointConstituents(points);
-	jet.GetConstituents(rhs);
-	int n_pts = points.GetNPoints();
-cout << n_pts << " constituents to cluster" << endl;	
-	
-	vgmm.AddData(&points);
-	vgmm.Initialize();
-	string fname = "jetTest"; 
-	//loop
-	double dLogL, newLogL, oldLogL;
-	////////run EM algo////////
-	for(int it = 0; it < maxNit; it++){
-		oldLogL = vgmm.EvalLogL();
-		
-		//E step
-		vgmm.Estimate();
-		//M step
-		vgmm.Update();
-		
-		//Check for convergence
-		newLogL = vgmm.EvalLogL();
-		if(isnan(newLogL)){
-			cout << "iteration #" << it << " log-likelihood: " << newLogL << endl;
-			return subjets;
-		}
-		dLogL = fabs(oldLogL - newLogL);
-		if(viz) cout << "iteration #" << it << " log-likelihood: " << newLogL << " dLogL: " << dLogL << endl;
-		if(dLogL < LogLthresh){
-			if(viz)
-			cout << "Reached convergence at iteration " << it << endl;
-			break;
-		}
-	}
-	int nsubjets = vgmm.GetNClusters(LogLthresh*10.);
+	/*	
+	//find maxK points with biggest energy - 4th dim
+	points.Sort(3);
+	PointCollection seeds;
+	PointCollection newpts;
+	vector<double> pt;
+	for(int k = 0; k < points.GetNPoints(); k++){
+		//discard energy dimension
+		pt.push_back(points.at(k).at(0));
+		pt.push_back(points.at(k).at(1));
+		pt.push_back(points.at(k).at(2));
 
-	//TODO: consider edge case where r_nk = r_nk' for all k == k' (all k entries for a point are equal)
-	//assign points to found subjets (clusters)
-	for(int k = 0; k < nsubjets; k++){
-		subjets.push_back(Jet());
+		if(k >= points.GetNPoints() - maxK) seeds += Point(pt);
+		newpts += Point(pt);
+		pt.clear();
 	}
-	int k_assign;
-	for(int n = 0; n < n_pts; n++){
-		//find k where post_nk is max for point n:
-		k_assign = vgmm.GetMaxPointAssignment(n);
-		//assign point n to subjet k - subjets[postMax_k] += rh[n];
-		subjets[k_assign].add(rhs[n]);
-	}
-*/
-	
+	points.Clear();
+	*/
+	GaussianMixture* gmm = FindSubjets(points, thresh, maxNit, maxK, viz, a);
+	int nsubjets = gmm->GetNClusters();
+
+
 	return subjets;
 }
 
