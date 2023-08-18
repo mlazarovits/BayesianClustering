81bb444865b7cc0f9c1b889a572c697b87fdc418
Margaret Lazarovits <mlazarovits@gmail.com>
2023-08-18 16:15:35 -0500

    changed inheritance back to public

--- include/FullViz3D.hh
+++ include/FullViz3D.hh
@@ -12,7 +12,7 @@ using std::string;
 
 
 using node = BaseTree::node;
-class FullViz3D : ClusterVizBase{
+class FullViz3D : public ClusterVizBase{
 	public:
 		FullViz3D() : ClusterVizBase(){ };
 		FullViz3D(vector<node*> nodes);
