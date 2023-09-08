0b9486d94ebde0bf81ef8f6612f490229b65d48b
Margaret Lazarovits <mlazarovits@gmail.com>
2023-09-07 21:05:42 -0500

    updated signal label for legend

--- include/PhotonSkimmer.hh
+++ include/PhotonSkimmer.hh
@@ -37,7 +37,7 @@ class PhotonSkimmer : public BaseSkimmer{
 		void MakeIDHists(){
 			//signal
 			plotCat sig;
-			sig.legName = "#Chi #rightarrow #gamma";
+			sig.legName = "#Chi^0 #rightarrow #gamma";
 			sig.plotName = "chiGam";
 			sig.ids = {22, 32, 25, 35};
 			plotCats.push_back(sig);
