10008eea35c61807788c58127ede7c868083d7cd
Margaret Lazarovits <mlazarovits@gmail.com>
2023-09-08 10:52:01 -0500

    updated + debug

--- include/JetSkimmer.hh
+++ include/JetSkimmer.hh
@@ -2,10 +2,13 @@
 #define JETSKIMMER_HH
 
 #include "JetPoint.hh"
-#include "TFile.h"
 #include "BaseSkimmer.hh"
 #include "BasePDFMixture.hh"
+#include <TFile.h>
+#include "JetProducer.hh"
+#include "TSystem.h"
 
+using plotCat = BaseSkimmer::plotCat;
 class JetSkimmer : public BaseSkimmer{
 	public:
 		JetSkimmer();
@@ -19,6 +22,10 @@ class JetSkimmer : public BaseSkimmer{
 		void CleaningSkim(){ };
 		void Skim();
 
+		//jet specific quantities
+		TH1D* nClusters = new TH1D("nClusters","nClusters",20,0,20);
+		TH1D* nTrueJets = new TH1D("nTrueJets","nTrueJets",20,0,20);
+
 
 		//all hists referenced here are in hists1D
 		void FillTotalHists(BasePDFMixture* model){
@@ -30,6 +37,8 @@ class JetSkimmer : public BaseSkimmer{
 			//e_nSubClusters->Fill(_base->Photon_energy->at(p), nclusters);
 			//model->GetAvgWeights(avg_Es);		
 			double theta, phi, r, id;
+		
+			nClusters->Fill((double)nclusters);
 			for(int k = 0; k < nclusters; k++){
 				params = model->GetParameters(k);
 				eta_center->Fill(params["mean"].at(0,0));
@@ -59,6 +68,22 @@ class JetSkimmer : public BaseSkimmer{
 				//average cluster energy
 				e_avg->Fill(avg_Es[k]);
 			}
+		}
+		void WriteHists(TFile* ofile){
+			string name;
+		
+
+			ofile->cd();
+			for(int i = 0; i < (int)hists1D.size(); i++){
+				TCanvas* cv = new TCanvas(name.c_str(), "");
+				TDRHist(hists1D[i], cv, name, name, "a.u.");
+				//write cv to file			
+			//	cv->SaveAs((fname+"/"+name+".pdf").c_str());
+				cv->Write();
+
+			}
+			ofile->Close();
+
 		}
 
 
