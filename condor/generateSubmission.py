2e1e6b0b4080b9e7100d9a8dfc36c4fc486b685f
mlazarovits <mrlazarovits@gmail.com>
2023-11-30 21:34:59 -0600

    debugged condor submission for skims - tested jets so far need to test photons

--- condor/generateSubmission.py
+++ condor/generateSubmission.py
@@ -5,90 +5,98 @@
 
 import sys
 import os
+import argparse
 import shutil
 import submissionHelper as SH
-import argparse
-
-odir = 'Output/'
-#Load input arguments ()
-parser = argparse.ArgumentParser()
-#Ntuple file to run over
-parser.add_argument('--inputFile','-i',help='Ntuple file to create skims from (absolute path)',required=True)
-#which object to analyze (jets or photons currently supported)
-parser.add_argument('--object','-o',help='which object to skim (currently only jets or photons supported)',choices=["jets","photons"],required=True)
-parser.add_argument('--strategy','-st',help='if skimming jets, which strategy to use for BHC (NlnN = 0 default, N2 = 1)',default=0,type=int,choices=["1","0"])
-parser.add_argument('--split','-s',help="condor job split",default=0,type=int)
-parser.add_argument('--verbosity','-v',help="verbosity",default=0)
-#add algorithm parameters - alpha, emAlpha, verbosity, thresh
-parser.add_argument('--alpha','-a',help="alpha for BHC",default=0.1)
-parser.add_argument('--EMalpha','-EMa',help="alpha for GMM (EM algo)",default=0.5)
-parser.add_argument('--thresh','-t',help='threshold for GMM clusters',default=1.)
-args = parser.parse_args()
 
 
+# Create workspace and condor submit files.
+def generateSubmission(args):
+ 	# Ensure that the directory includes "/" at the end.
+	odir = args.directory
+	if odir[-1] != "/":
+		odir += "/"
+	
+	print("Directory for condor submission: {0}".format(odir))
+	print("------------------------------------------------------------")
+	# Create output directory for condor results if it does not exist.
+	SH.makeDir(odir)
+	
+	inputFile = args.inputFile
+	objName = args.object
+	strategyName = ""
+	if args.strategy == 0:
+	        strategyName = "NlnN"
+	elif args.strategy == 1:
+	        strategyName = "N2"
+	else:
+	        print("Invalid strategy",args.strategy,"specified")
+	        exit()
+	
+	print "Skimming for",objName,"in file",args.inputFile 
+	
+	
+	#organize output by sample, object (ie jets or photons), and strategy (for jets only - NlnN or N2)
+	#find .root and then find the / before that (if it exists) - everything in between is the file name
+	sampleName = inputFile[ inputFile.rfind("/")+1 : inputFile.find(".root") ]
+	sampleNameShort = sampleName[ : sampleName.find("_") ]
+	
+	dirname = ""
+	ofilename = ""
+	if(objName == "jets"):
+	        dirname = odir+sampleNameShort+"/"+sampleName+"/"+objName+"/"+strategyName
+	        ofilename = sampleNameShort+"_"+objName+"_"+strategyName
+	#strategy doesn't apply to photons (GMM only)
+	else:
+	        dirname = odir+"/"+sampleNameShort+"/"+sampleName+"/"+objName
+	        ofilename = dirname+"/out/"+sampleNameShort+"_"+objName+"_"+strategyName
 
+	
+	print("Preparing sample directory: {0}".format(dirname))
 
+	##### Create a workspace (remove existing directory) #####
+	if os.path.exists(dirname):
+		print("Removing existing directory: {0}".format(dirname))
+		shutil.rmtree(dirname)
 
-inputFile = args.inputFile
-objName = args.object
-strategyName = ""
-if args.strategy == 0:
-	strategyName = "NlnN"
-elif args.strategy == 1:
-	strategyName = "N2"
-else:
-	print("Invalid strategy",args.strategy,"specified")
-	exit()
+	# Create directories for work area.
+	SH.createWorkArea(dirname)
 
-print "Skimming for",objName,"in file",args.inputFile
+	# grab relevant flags
+	eventnums = SH.eventsSplit(inputFile, args.split)
+	flags = '--alpha '+str(args.alpha)+' --EMalpha '+str(args.EMalpha)+' -v '+str(args.verbosity)+' -t '+str(args.thresh)+" -s "+str(args.strategy)
 
+	##### Create condor submission script in src directory #####
+	condorSubmitFile = dirname + "/src/submit.sh"
+	subf = open(condorSubmitFile, "w")
+	oFileName = "condorSkim"
+	SH.writeSubmissionBase(subf, dirname, oFileName, inputFile)
+	SH.writeQueueList(subf, inputFile, oFileName, eventnums, flags)
+	#subf.close()
+	
+	print("------------------------------------------------------------")
+	print("Submission ready, to run use:")
+	print("condor_submit "+condorSubmitFile)
 
-#organize output by sample, object (ie jets or photons), and strategy (for jets only - NlnN or N2)
-#find .root and then find the / before that (if it exists) - everything in between is the file name
-sampleName = inputFile[ inputFile.rfind("/")+1 : inputFile.find(".root") ]
-sampleNameShort = sampleName[ : sampleName.find("_") ]
+def main():
+	# options
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--directory", "-d", default="Output", help="working directory for condor submission")
+	
+	#Ntuple file to run over
+	parser.add_argument('--inputFile','-i',help='Ntuple file to create skims from (absolute path)',required=True)
+	#which object to analyze (jets or photons currently supported)
+	parser.add_argument('--object','-o',help='which object to skim (currently only jets or photons supported)',choices=["jets","photons"],required=True)
+	parser.add_argument('--strategy','-st',help='if skimming jets, which strategy to use for BHC (NlnN = 0 default, N2 = 1)',default=0,type=int,choices=["1","0"])
+	parser.add_argument('--split','-s',help="condor job split",default=0,type=int)
+	parser.add_argument('--verbosity','-v',help="verbosity",default=0)
+	#add algorithm parameters - alpha, emAlpha, verbosity, thresh
+	parser.add_argument('--alpha','-a',help="alpha for BHC",default=0.1)
+	parser.add_argument('--EMalpha','-EMa',help="alpha for GMM (EM algo)",default=0.5)
+	parser.add_argument('--thresh','-t',help='threshold for GMM clusters',default=1.)
+	args = parser.parse_args()
 
-dirname = ""
-ofilename = ""
-if(objName == "jets"):
-	dirname = odir+sampleNameShort+"/"+sampleName+"/"+objName+"/"+strategyName
-	ofilename = sampleNameShort+"_"+objName+"_"+strategyName
-#strategy doesn't apply to photons (GMM only)
-else:
-	dirname = odir+"/"+sampleNameShort+"/"+sampleName+"/"+objName
-	ofilename = dirname+"/out/"+sampleNameShort+"_"+objName+"_"+strategyName
-#####################Create a workspace (remove existing directory)############
-if os.path.exists(dirname):
-	print "removing existing directory",dirname
-	shutil.rmtree(dirname)
-os.makedirs(dirname)
-os.makedirs(dirname+"/src")
-os.makedirs(dirname+"/log")
-os.makedirs(dirname+"/out")
+	generateSubmission(args)
 
-
-####################Create submission script in src directory##################
-subf = open(dirname+"/src/submit.sh","w")
-SH.writeSubmissionBase( subf, dirname, inputFile )
-eventnums = SH.eventsSplit(inputFile, args.split)
-flags = '--alpha '+str(args.alpha)+' --EMalpha '+str(args.EMalpha)+' -v '+str(args.verbosity)+' -t '+str(args.thresh)+" -s "+str(args.strategy) 
-SH.writeQueueList(subf, inputFile, dirname+"/out/skims", eventnums, flags)
-
-print("submission ready, to run use:")
-#need to be in directory with the execution script to run
-print("condor_submit "+dirname+"/src/submit.sh")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
---
+++ condor/submissionHelper_old.py
@@ -0,0 +1,58 @@
+#############################
+#Thanks to Justin Anguiano (2023) for the basis 
+#for this condor submission script generating code
+#############################
+import ROOT
+import os
+import re
+import numpy as np
+def writeSubmissionBase( subf, dirname, infile ):
+	subf.write("universe = vanilla\n")
+	subf.write("executable = execute_script.sh\n")
+	subf.write("output = ./"+dirname+"/log/job.$(Process).out\n")
+	subf.write("error = ./"+dirname+"/log/job.$(Process).err\n")
+	subf.write("log = ./"+dirname+"/log/job.log\n")
+	#include tarball with CMSSW environment
+	subf.write("transfer_input_files = /uscms/home/z374f439/nobackup/whatever_you_want/sandbox-CMSSW_10_6_5-6403d6f.tar.bz2, config.tgz, "+infile+"\n")
+	subf.write("should_transfer_files = YES\n")
+	subf.write("when_to_transfer_output = ON_EXIT\n")
+	outname = dirname+"/out/skim.$(Process).root"
+	subf.write("transfer_output_files = "+outname+"\n")
+	# need to supply absolute path for remap
+	#absCWD = os.path.abspath(".") # these cwd give the wrong abs path, there is something special in the environment
+	#absCWD = os.getcwd()
+	absCWD = os.popen('pwd').readline().rstrip() 
+	print("abs path is "+ absCWD)
+	remap= absCWD+"/"+outname
+	print("remap is "+ remap)
+	subf.write("transfer_output_remaps = \""+outname+"="+remap+"\"\n")
+
+#splits by event number
+def eventsSplit(infile, nChunk):
+	if nChunk == 0:
+		nChunk += 1
+	print("Splitting each file into "+str(nChunk)+" jobs ")
+	#should split by event number in file
+	rfile = ROOT.TFile(infile)
+	nevts = rfile.Get("tree/llpgtree").GetEntries()
+	evts = range(nevts+1)
+	#return array of pairs of evtFirst and evtLast to pass as args into the exe to run
+	arr = [[min(i), max(i)] for i in np.array_split(evts,nChunk)] 
+	return arr
+
+def writeQueueList( subf, inFile, ofilename, evts, flags ):
+	outFileArg = " -o "+ofilename+".$(Process).root"
+
+	#infile should only be file name (no path)
+	inFile = inFile[inFile.rfind("/")+1:]
+	
+	jobCtr=0
+	for e in evts:
+		inFileArg = " -i "+inFile
+		Args = "Arguments ="+inFileArg+" "+flags+" --evtFirst "+str(e[0])+" --evtLast "+str(e[1])+" -o "+ofilename+"\n"
+		subf.write("\n\n\n")
+		subf.write("###### job"+str(jobCtr)+ "######\n")
+		subf.write(Args)
+		subf.write("Queue\n")	
+		jobCtr=jobCtr+1
+
