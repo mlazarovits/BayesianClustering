ae98dc5d182bd9c9bed81c3678f05a334571d8c8
mlazarovits <mrlazarovits@gmail.com>
2023-12-29 17:23:03 -0600

    removed print

--- condor/doHadd.py
+++ condor/doHadd.py
@@ -6,7 +6,6 @@ def main():
 	parser.add_argument("--dir","-d",help="top directory that has directories of root files to hadd i.e. Output/GMSB/GMSB_AOD_v13_GMSB_L-150TeV_Ctau-200cm_AODSIM_RunIIFall17DRPremix/jets/",required=True)
 	parser.add_argument("--force",help='force remake of outfile',action='store_true')
 	args = parser.parse_args()
-	print("force? "+str(args.force))
 
 	cmd = "hadd -d /uscmst1b_scratch/lpc1/3DayLifetime/mlazarov/ -j 4"
 	for d in os.scandir(args.dir):
