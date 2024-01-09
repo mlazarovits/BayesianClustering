359846978d60edaa0414293db023e206003caaa4
Margaret Lazarovits <mlazarovits@gmail.com>
2024-01-09 17:19:00 -0600

    changed names of profiles

--- src/BaseSkimmer.cc
+++ src/BaseSkimmer.cc
@@ -6,13 +6,18 @@
 void BaseSkimmer::Profile2DHist(TH2D* inhist, TH1D* outhist, vector<TH1D*>& profs){
 	int nbins = inhist->GetNbinsX();
 	profs.clear();
+	string profilename = "";
 	//skip overflow + underflow bins
 	for(int i = 1; i < nbins; i++){
 		TH1D* phist = (TH1D*)inhist->ProjectionY("tmp",i,i);
-		phist->SetTitle(("profile_bin"+std::to_string(i)).c_str());	
-		phist->SetName(("profile_bin"+std::to_string(i)).c_str());
+		profilename = inhist->GetName();
+		profilename = profilename.substr(0,profilename.rfind("_"));
+		profilename = profilename.substr(0,profilename.rfind("_"));
+		phist->SetTitle(("profile_"+profilename+"_bin"+std::to_string(i)).c_str());	
+		phist->SetName(("profile_"+profilename+"_bin"+std::to_string(i)).c_str());
 		phist->GetXaxis()->SetTitle(inhist->GetXaxis()->GetTitle());	
 		profs.push_back(phist);
+
 		outhist->GetXaxis()->SetTitle(inhist->GetXaxis()->GetTitle());
 		string ytitle = inhist->GetYaxis()->GetTitle();
 		ytitle = "#sigma "+ytitle;
