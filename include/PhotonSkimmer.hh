5910c02a5045bc3811e288278fac40ee642aa0b0
Margaret Lazarovits <mlazarovits@gmail.com>
2024-03-29 11:21:38 +0100

    removed any Scale lines (even commented out ones) because any scaling needs to happen after all files have been hadded

--- include/PhotonSkimmer.hh
+++ include/PhotonSkimmer.hh
@@ -1288,8 +1288,6 @@ class PhotonSkimmer : public BaseSkimmer{
 
 			//write 2D hists
 			for(int i = 0; i < (int)hists2D.size(); i++){
-				//normalize etaphiCov_timeetaCov count plots
-				//pc.hists2D[i][68]->Scale(1./pc.hists2D[i][68]->Integral());
 				for(int j = 0; j < hists2D[i].size(); j++){
 					if(pc.hists2D[i][j] == nullptr) continue;
 					//write total hist to file
