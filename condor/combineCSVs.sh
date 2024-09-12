107df283ad4b08f2f4fa3419260c09477f80b261
mlazarovits <mrlazarovits@gmail.com>
2024-09-12 09:58:41 -0500

    run over multiple dirs with *

--- condor/combineCSVs.sh
+++ condor/combineCSVs.sh
@@ -3,13 +3,16 @@ then
 	echo "Please pass directory with out/*.csv files to concatenate"
 	return
 fi
-echo "Combining .csv files in" $1
-part1=$(dirname "$1")
-part2=$(basename "$part1")
-part3=$(basename "$1")
-
-outname=$1$part2"_"$part3
-head -n 1 $1/out/*.0.csv > $outname.csv
-tail -n+2 -q $1/out/*.csv >> $outname.csv
-
-echo "Wrote to" $outname".csv"
+for i in "$@"
+do
+	echo "Combining .csv files in $i"
+	part1=$(dirname "$i")
+	part2=$(basename "$part1")
+	part3=$(basename "$i")
+	
+	outname=$i$part2"_"$part3
+	head -n 1 $i/out/*.0.csv > $outname.csv
+	tail -n+2 -q $i/out/*.csv >> $outname.csv
+	
+	echo "Wrote to" $outname".csv"
+done
