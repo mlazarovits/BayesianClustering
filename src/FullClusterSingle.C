77faa90697753bdbdba6f8f06b7df5bfbf09b6c4
Margaret Lazarovits <mlazarovits@gmail.com>
2025-01-10 16:03:46 -0600

    added option to change prior params from cmd line

--- src/FullClusterSingle.C
+++ src/FullClusterSingle.C
@@ -26,8 +26,17 @@ int main(int argc, char *argv[]){
 	string in_file = "rootfiles/GMSB_AOD_v9_GMSB_L-350TeV_Ctau-200cm_AODSIM_RunIIFall17DRPremix-PU2017_94X_output99.root";
 	bool hprint = false;
 	double thresh = 1.;
-	double alpha = 0.1;
+	
+	//prior parameters
 	double emAlpha = 0.5;
+	Matrix scale = Matrix(1e-3);
+	Matrix dof = Matrix(3);
+	Matrix W(3,3);
+	W.InitIdentity();
+	W.mult(W,1./3.);
+	Matrix m(3,1);
+	double alpha = 0.1;
+
 	bool viz = false;
 	int verb = 0;
 	bool weighted = true;
@@ -43,6 +52,7 @@ int main(int argc, char *argv[]){
 	//this should be in N/GeV
 	//at least at 1 GeV but 1 GeV rh shouldn’t be able to seed a cluster so 1 GeV should be a fraction of entries
 	double gev = 1./30.;
+	double minRhE = 0.5;
 	for(int i = 0; i < argc; i++){
 		if(strncmp(argv[i],"--help", 6) == 0){
     	 		hprint = true;
@@ -102,6 +112,41 @@ int main(int argc, char *argv[]){
 			i++;
     	 		alpha = std::stod(argv[i]);
    		}
+		if(strncmp(argv[i],"--beta0", 7) == 0){
+			i++;
+    	 		double beta0 = std::stod(argv[i]);
+			scale = Matrix(beta0);
+   		}
+		if(strncmp(argv[i],"--nu0", 5) == 0){
+			i++;
+    	 		double nu0 = std::stod(argv[i]);
+			dof = Matrix(nu0);
+   		}
+		if(strncmp(argv[i],"--W0diag", 8) == 0){
+			i++;
+    	 		double W_ee = std::stod(argv[i]);
+			i++;
+    	 		double W_pp = std::stod(argv[i]);
+			i++;
+    	 		double W_tt = std::stod(argv[i]);
+			//no covariance bw dimensions
+			W = Matrix(3,3);
+			W.SetEntry(W_ee,0,0);
+			W.SetEntry(W_pp,1,1);
+			W.SetEntry(W_tt,2,2);
+   		}
+		if(strncmp(argv[i],"--m0", 4) == 0){
+			i++;
+    	 		double m_e = std::stod(argv[i]);
+			i++;
+    	 		double m_p = std::stod(argv[i]);
+			i++;
+    	 		double m_t = std::stod(argv[i]);
+			m = Matrix(3,1);
+			m.SetEntry(m_e,0,0);
+			m.SetEntry(m_p,1,0);
+			m.SetEntry(m_t,2,0);
+   		}
 		if(strncmp(argv[i],"--noWeight", 10) == 0){
     	 		weighted = false;
    		}
@@ -142,29 +187,38 @@ int main(int argc, char *argv[]){
 			i++;
     	 		gev = std::stod(argv[i]);
    		}
+		if(strncmp(argv[i],"--minRhE", 8) == 0){
+			i++;
+    	 		minRhE = std::stod(argv[i]);
+   		}
 		
 
 	}
 	if(hprint){
 		cout << "Usage: " << argv[0] << " [options]" << endl;
    		cout << "  options:" << endl;
-   		cout << "   --help(-h)                    print options" << endl;
-   		cout << "   --input(-i) [file]            input root file" << endl;
-   		cout << "   --output(-o) [file]           output root file (in plots/)" << endl;
-   		cout << "   --alpha(-a) [a]               sets concentration parameter alpha for DPM in BHC (default = 0.1)" << endl;
-   		cout << "   --EMalpha(-EMa) [a]           sets concentration parameter alpha for variational EM GMM (default = 0.5)" << endl;
-   		cout << "   --thresh(-t) [t]              sets threshold for cluster cutoff" << endl;
-   		cout << "   --verbosity(-v) [verb]        set verbosity (default = 0)" << endl;
-   		cout << "   --strategy(-s) [strat]        set clustering strategy for skimmer (0 : NlnN (default), 1 : N^2,  does not apply to photons)" << endl;
-   		cout << "   --object [obj]                set object to cluster (0 : jets, default; 1 : photons, 2 : detector sim)" << endl;
-   		cout << "   --nobj [n]                    set number of object in event to cluster (photons only, default = 0)" << endl;
-   		cout << "   --evt(-e) [evt]               get plots for event e (default = 0)" << endl;
-   		cout << "   --gev [gev]                   set energy weight transfer factor in N/GeV (default = 1/30 GeV)" << endl;
-   		cout << "   --viz                         makes plots (and gifs if N == 3)" << endl;
-   		cout << "   --noSmear                     turns off smearing data according to preset covariance (default = true)" << endl;
-   		cout << "   --timeSmear                   turns on time smearing data according to preset covariance (default = false)" << endl;
-   		cout << "   --noWeight                    turns off weighting data points (default = true)" << endl;
-   		cout << "   --noDist                      turns off - clusters must be within pi/2 in phi (default = true)" << endl;
+   		cout << "   --help(-h)                           print options" << endl;
+   		cout << "   --input(-i) [file]                   input root file" << endl;
+   		cout << "   --output(-o) [file]                  output root file (in plots/)" << endl;
+   		cout << "   --alpha(-a) [a]                      sets concentration parameter alpha for DPM in BHC (default = 0.1)" << endl;
+   		cout << "   --EMalpha(-EMa) [a]                  sets concentration parameter alpha for variational EM GMM (default = 0.5)" << endl;
+   		cout << "   --thresh(-t) [t]                     sets threshold for cluster cutoff" << endl;
+   		cout << "   --beta0 [beta0]                      set scale parameter on covariance for prior on mu (N(mu | m0, (beta0*Lambda)^-1) (default = 0.001)" << endl;
+   		cout << "   --m0 [m0_eta] [m0_phi] [m0_time]     set mean parameter for prior on mu (N(mu | m0, (beta0*Lambda)^-1) (default = [0,0,0])" << endl;
+   		cout << "   --W0diag [W0_ee] [W0_pp] [W0_tt]     set *diagonal elements* in covariance parameter for prior on lambda (InverseWishart(Lambda | W0, nu0) (default = [1/3,1/3,1/3])" << endl;
+   		cout << "   --nu0 [nu0]                          set dof parameter for prior on lambda (InverseWishart(Lambda | W0, nu0) (default = 3 = dim)" << endl;
+   		cout << "   --verbosity(-v) [verb]               set verbosity (default = 0)" << endl;
+   		cout << "   --strategy(-s) [strat]               set clustering strategy for skimmer (0 : NlnN (default), 1 : N^2, 2 : GMM only, 0+1 do not apply to photons)" << endl;
+   		cout << "   --object [obj]                       set object to cluster (0 : jets, default; 1 : photons, 2 : detector sim)" << endl;
+   		cout << "   --nobj [n]                           set number of object in event to cluster (photons only, default = 0)" << endl;
+   		cout << "   --evt(-e) [evt]                      get plots for event e (default = 0)" << endl;
+   		cout << "   --gev [gev]                          set energy weight transfer factor in N/GeV (default = 1/30 GeV)" << endl;
+   		cout << "   --minRhE [minRhe]                    set minimum ECAL rechit energy (default = 0.5 GeV)" << endl;
+   		cout << "   --viz                                makes plots (and gifs if N == 3)" << endl;
+   		cout << "   --noSmear                            turns off smearing data according to preset covariance (default = true)" << endl;
+   		cout << "   --timeSmear                          turns on time smearing data according to preset covariance (default = false)" << endl;
+   		cout << "   --noWeight                           turns off weighting data points (default = true)" << endl;
+   		cout << "   --noDist                             turns off - clusters must be within pi/2 in phi (default = true)" << endl;
    		cout << "Example: ./FullClusterSingle.x -a 0.5 -t 1.6 --viz" << endl;
 
    		return 0;
@@ -173,6 +227,14 @@ int main(int argc, char *argv[]){
 	/////GET DATA FROM NTUPLE//////
 	string cmslab = in_file.substr(0,in_file.find(".root"));
 	cmslab = cmslab.substr(cmslab.find("/")+1);
+	if(cmslab.find("cmseos") != string::npos)
+		cmslab = "";
+	
+	map<string, Matrix> prior_params;
+	prior_params["scale"] = scale;
+	prior_params["dof"] = dof;
+	prior_params["scalemat"] = W;
+	prior_params["m"] = m;
 
 	if(obj == 0 || obj == 2){
 		if(obj == 0) fname += "jets";
@@ -207,6 +269,7 @@ int main(int argc, char *argv[]){
 	a_string = stream.str();
 	int idx = a_string.find(".");
 	a_string.replace(idx,1,"p");	
+	if(strat != 2) fname += "_bhcAlpha"+a_string;
 	
 	string ema_string;
 	stream.str("");
@@ -214,13 +277,66 @@ int main(int argc, char *argv[]){
 	ema_string = stream.str();
 	idx = ema_string.find(".");
 	ema_string.replace(idx,1,"p");	
+	fname += "_emAlpha"+ema_string+"_";
+		
+	string scale_string;
+	stream.str("");
+	stream << std::fixed << std::setprecision(3) << scale.at(0,0);
+	scale_string = stream.str();
+	idx = scale_string.find(".");
+	scale_string.replace(idx,1,"p");	
+	fname += "beta0-"+scale_string+"_";
+	
+	string mean_string;
+	stream.str("");
+	stream << std::fixed << std::setprecision(3);
+	for(int i = 0; i < m.GetDims()[0]; i++){
+		stream << m.at(i,0);
+		if(i < m.GetDims()[0]-1) stream << "-";
+	}
+	mean_string = stream.str();
+	idx = 0;
+	while(idx != string::npos){
+		idx = mean_string.find(".");
+		mean_string.replace(idx,1,"p");	
+		idx = mean_string.find(".");
+	}
+	fname += "m0-"+mean_string+"_";
+	
+	string W_string;
+	stream.str("");
+	stream << std::fixed << std::setprecision(3);
+	for(int i = 0; i < W.GetDims()[0]; i++){
+		stream << W.at(i,i);
+		if(i < W.GetDims()[0]-1) stream << "-";
+	}
+	W_string = stream.str();
+	idx = 0;
+	while(idx != string::npos){
+		idx = W_string.find(".");
+		W_string.replace(idx,1,"p");	
+		idx = W_string.find(".");
+	}
+	//do replacement of . to p
+	fname += "W0diag-"+W_string+"_";
+	
+	string dof_string;
+	stream.str("");
+	stream << std::fixed << std::setprecision(3) << dof.at(0,0);
+	dof_string = stream.str();
+	idx = dof_string.find(".");
+	dof_string.replace(idx,1,"p");	
+	fname += "nu0-"+dof_string+"_";
+
 
 	string t_string;
 	stream.str("");
 	stream << std::fixed << std::setprecision(3) << thresh;
 	t_string = stream.str();
 	idx = t_string.find(".");
-	t_string.replace(idx,1,"p");	
+	t_string.replace(idx,1,"p");
+	fname += "thresh"+t_string+"_";
+
 	
 	string gev_string;
 	stream.str("");
@@ -228,19 +344,24 @@ int main(int argc, char *argv[]){
 	gev_string = stream.str();
 	idx = gev_string.find(".");
 	gev_string.replace(idx,1,"p");	
-
-	
-	if(obj != 1) fname += "_bhcAlpha"+a_string+"_emAlpha"+ema_string+"_thresh"+t_string+"_";
-	else fname += "_emAlpha"+ema_string+"_thresh"+t_string+"_";
-
 	fname += "NperGeV"+gev_string+"_";
+	
 	fname += cmslab; //long sample name
-	fname += "_evt"+to_string(evt);
+	fname += "evt"+to_string(evt);
 
 	fname = "plots/"+fname;
 
-
-
+	cout << "Prior Parameters" << endl;
+	cout << "beta0" << endl;
+	scale.Print();
+	cout << "mean0" << endl;
+	m.Print();
+	cout << "nu0" << endl;
+	dof.Print();
+	cout << "W0" << endl;
+	W.Print(); 
+	cout << "fname " << fname << endl;
+	
 	if(viz){
 		cout << "Writing to directory: " << fname << endl;
 		if(gSystem->AccessPathName(fname.c_str())){
@@ -302,7 +423,7 @@ int main(int argc, char *argv[]){
 		JetSimProducer prod(file);
 		prod.SetTransferFactor(gev);
 		prod.SetRecoPtMin(20);
-		prod.SetMinRhE(0.5); 
+		prod.SetMinRhE(minRhE); 
 		//skimmer.SetTransferFactor(gev);
 		//skimmer.SetAlpha(alpha);
 		//skimmer.SetSubclusterAlpha(emAlpha);
@@ -344,6 +465,7 @@ int main(int argc, char *argv[]){
 	algo->SetAlpha(alpha);
 	algo->SetSubclusterAlpha(emAlpha);
 	algo->SetVerbosity(verb);
+	algo->SetPriorParameters(prior_params);
 	clock_t t = clock();
 	//jets
 	if(obj == 0 || obj == 2){
@@ -359,6 +481,7 @@ int main(int argc, char *argv[]){
 				//to track computation time from beginning of program
 				if(obj == 0) trees = algo->N2Cluster();
 			}
+			cout << trees.size() << " true trees" << endl;
 			if(viz){
 				//plotting stuff here
 				FullViz3D plots = FullViz3D(trees);
@@ -368,7 +491,6 @@ int main(int argc, char *argv[]){
 				plots.AddTrueJets(jets);
 				plots.Write(fname);
 			}
-			cout << jets.size() << " true jets" << endl;
 		}
 		else if(strat == 2){
 			cout << "mixture model with pre-clustered AK4 jets" << endl;
