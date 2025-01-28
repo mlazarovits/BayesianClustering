2c17a94f4d3424b2b005409a880c487a6591ed9d
Margaret Lazarovits <mlazarovits@gmail.com>
2025-01-28 13:01:44 -0600

    added vector<vector<int>> to dictionary

--- LinkDef.h
+++ LinkDef.h
@@ -1,4 +1,6 @@
 #ifdef __CLING__
 #pragma link C++ class vector<unsigned int> +;
 #pragma link C++ class vector<vector<unsigned int>> +;
+#pragma link C++ class vector<int> +;
+#pragma link C++ class vector<vector<int>> +;
 #endif
