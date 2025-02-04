712e601a32fb7d1f928499bdfd997ba2512ee53d
mlazarovits <mrlazarovits@gmail.com>
2025-02-04 11:37:59 -0600

    removed .root from file name in EventWeight output

--- macros/EventWeightCalc.C
+++ macros/EventWeightCalc.C
@@ -31,25 +31,25 @@ int EventWeightCalc(string selection = ""){
 
 	//just for MCs - data weight = 1
 	vector<string> files;
-	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-40To100_AODSIM_RunIIFall17DRPremix.root");
-	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-100To200_AODSIM_RunIIFall17DRPremix.root");
-	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-200To400_AODSIM_RunIIFall17DRPremix.root");
-	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-400To600_AODSIM_RunIIFall17DRPremix.root");
-	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-600ToInf_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-150TeV_Ctau-0_1cm_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-150TeV_Ctau-200cm_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-350TeV_Ctau-0_1cm_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-350TeV_Ctau-1000cm_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-350TeV_Ctau-200cm_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT1000to1500_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT100to200_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT1500to2000_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT2000toInf_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT200to300_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT300to500_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT500to700_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT50to100_AODSIM_RunIIFall17DRPremix.root");
-	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT700to1000_AODSIM_RunIIFall17DRPremix.root");
+	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-40To100_AODSIM_RunIIFall17DRPremix");
+	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-100To200_AODSIM_RunIIFall17DRPremix");
+	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-200To400_AODSIM_RunIIFall17DRPremix");
+	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-400To600_AODSIM_RunIIFall17DRPremix");
+	files.push_back("GJets_R17"+selection+"_v24_GJets_HT-600ToInf_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-150TeV_Ctau-0_1cm_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-150TeV_Ctau-200cm_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-350TeV_Ctau-0_1cm_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-350TeV_Ctau-1000cm_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("GMSB_R17"+selection+"_v24_GMSB_L-350TeV_Ctau-200cm_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT1000to1500_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT100to200_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT1500to2000_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT2000toInf_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT200to300_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT300to500_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT500to700_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT50to100_AODSIM_RunIIFall17DRPremix");
+	//files.push_back("QCD_R17"+selection+"_v24_QCD_HT700to1000_AODSIM_RunIIFall17DRPremix");
 	ofstream ofile;
 	string ofilename = "info/EventWeights"+selection+".txt";
 	ofile.open(ofilename);
@@ -58,9 +58,9 @@ int EventWeightCalc(string selection = ""){
 	for(int f = 0; f < files.size(); f++){
 		cout << "File " << files[f];
 		//loop over all files in a list
-		TFile* file = TFile::Open(("root://cmseos.fnal.gov//store/user/lpcsusylep/malazaro/KUCMSNtuples/"+files[f]).c_str());
+		TFile* file = TFile::Open(("root://cmseos.fnal.gov//store/user/lpcsusylep/malazaro/KUCMSNtuples/"+files[f]+".root").c_str());
 		JetProducer* jet_prod = new JetProducer(file);
-		TFile* file2 = TFile::Open(("root://cmseos.fnal.gov//store/user/lpcsusylep/malazaro/KUCMSNtuples/"+files[f]).c_str());
+		TFile* file2 = TFile::Open(("root://cmseos.fnal.gov//store/user/lpcsusylep/malazaro/KUCMSNtuples/"+files[f]+".root").c_str());
 		PhotonProducer* pho_prod = new PhotonProducer(file2);
 		ReducedBase* base = jet_prod->GetBase();
 		int nEvts = base->fChain->GetEntries();
