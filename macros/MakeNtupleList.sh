e452ce0879533fe31f7b1e5e340ec0f33605ec87
mlazarovits <mrlazarovits@gmail.com>
2025-02-05 11:58:55 -0600

    running over multiple runs in sample

--- macros/MakeNtupleList.sh
+++ macros/MakeNtupleList.sh
@@ -6,7 +6,7 @@ fi
 PD=$1
 SEL=""
 NAME=""
-if [ $2 = "AL1IsoPho" ]
+if [ ! -z "$2" ]
 then
 	SEL=R17_$2_v24
 fi
@@ -28,28 +28,32 @@ else
 fi
 PREFIX=root://cmseos.fnal.gov/
 
+#if multiple runs in sample, do all runs
 FILE1=$(eosls /store/user/lpcsusylep/jaking/KUCMSNtuple/kucmsntuple_${PD}_${SEL}/${NAME}/)
-IFS="/" read -ra components1 <<< "$FILE1"
-DIR1="${components1[-1]}"
-
-FILE2=$(eosls /store/user/lpcsusylep/jaking/KUCMSNtuple/kucmsntuple_${PD}_${SEL}/${NAME}/${DIR1})
-IFS="/" read -ra components2 <<< "$FILE2"
-DIR2="${components2[-1]}"
-
-FILE3=$(eosls /store/user/lpcsusylep/jaking/KUCMSNtuple/kucmsntuple_${PD}_${SEL}/${NAME}/${DIR1}/${DIR2})
-
-outfile=filelists/${DIR1}_list.txt
-if [ -f ${outfile} ]; then
-	echo "Recreating file" $outfile
-	rm $outfile #start with a clean slate
-else
-	echo "Creating file" $outfile
-fi
-
-for index in ${FILE3[@]}; do
-	#eosls /store/user/lpcsusylep/jaking/KUCMSNtuple/kucmsntuple_${PD}_R17_${SEL}_v24/${NAME}/${DIR1}/${DIR2}/${index}/ >> ${outfile}
-	xrdfs ${PREFIX} ls /store/user/lpcsusylep/jaking/KUCMSNtuple/kucmsntuple_${PD}_${SEL}/${NAME}/${DIR1}/${DIR2}/${index}/ >> ${outfile}
-sed -i 's/^/root:\/\/cmseos.fnal.gov\//' ${outfile}
+components1=($FILE1)
+for dir in "${components1[@]}"
+do
+	DIR1=$dir
+	
+	FILE2=$(eosls /store/user/lpcsusylep/jaking/KUCMSNtuple/kucmsntuple_${PD}_${SEL}/${NAME}/${DIR1})
+	IFS="/" read -ra components2 <<< "$FILE2"
+	DIR2="${components2[-1]}"
+	
+	FILE3=$(eosls /store/user/lpcsusylep/jaking/KUCMSNtuple/kucmsntuple_${PD}_${SEL}/${NAME}/${DIR1}/${DIR2})
+	
+	outfile=filelists/${DIR1}_list.txt
+	if [ -f ${outfile} ]; then
+		echo "Recreating file" $outfile
+		rm $outfile #start with a clean slate
+	else
+		echo "Creating file" $outfile
+	fi
+	
+	for index in ${FILE3[@]}; do
+		#eosls /store/user/lpcsusylep/jaking/KUCMSNtuple/kucmsntuple_${PD}_R17_${SEL}_v24/${NAME}/${DIR1}/${DIR2}/${index}/ >> ${outfile}
+		xrdfs ${PREFIX} ls /store/user/lpcsusylep/jaking/KUCMSNtuple/kucmsntuple_${PD}_${SEL}/${NAME}/${DIR1}/${DIR2}/${index}/ >> ${outfile}
+		sed -i 's/^/root:\/\/cmseos.fnal.gov\//' ${outfile}
+	done
+	echo Wrote file list to $outfile
 done
-echo Wrote file list to $outfile
 
