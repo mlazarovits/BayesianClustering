3cc2b0b9d6e9180972cef8b590564081e5c64a6e
mlazarovits <mrlazarovits@gmail.com>
2025-05-07 14:24:41 -0500

    updated for new condor version - no longer support multiple Queue statements

--- condor/submissionHelper.py
+++ condor/submissionHelper.py
@@ -34,59 +34,37 @@ def writeSubmissionBase(subf, dirname, ofilename):
         subf.write("error = ./"+dirname+"/log/job.$(Process).err\n")
         subf.write("log = ./"+dirname+"/log/job.log\n")
         #include tarball with CMSSW environment
-        #subf.write("transfer_input_files = /uscms/home/z374f439/nobackup/whatever_you_want/sandbox-CMSSW_10_6_5-6403d6f.tar.bz2, config.tgz, \n")
-        subf.write("transfer_input_files = /uscms/home/mlazarov/nobackup/sandboxes/sandbox-CMSSW_13_0_13.tar.bz2, config.tgz, \n")
-        subf.write("MY.wantOS=\"el9\"\n")
+        #subf.write("transfer_input_files = /uscms/home/z374f439/nobackup/whatever_you_want/sandbox-CMSSW_10_6_5-6403d6f.tar.bz2, configSim.tgz, \n")
+        subf.write("transfer_input_files = /uscms/home/mlazarov/nobackup/sandboxes/sandbox-CMSSW_13_0_13.tar.bz2, configSim.tgz, \n")
         subf.write("should_transfer_files = YES\n")
+        #subf.write("request_memory = 4096\n")
         subf.write("when_to_transfer_output = ON_EXIT\n")
-        if "jet" in dirname:
-            subf.write("request_memory=4096\n")
-        outnames = []
-        outnames.append(ofilename+".$(Process).root")
-        #if photons, write csv file for MVA
-        if "photon" in dirname or "superclusters" in dirname:
-            outnames.append(ofilename+".$(Process).csv")
-        outname = ""
-        for o in outnames:
-            outname += o+", "
-        #remove last comma and space
-        outname = outname[:-2]
-        #subf.write("transfer_output_files = "+outname+"\n")
+        outname = ofilename+".$(Process).root"
         subf.write("transfer_output_files = "+outname+"\n")
         # need to supply absolute path for remap
         #absCWD = os.path.abspath(".") # these cwd give the wrong abs path, there is something special in the environment
         #absCWD = os.getcwd()
         absCWD = os.popen('pwd').readline().rstrip()
         #print("abs path is "+ absCWD)
-        remap = ""
-        for o in outnames:
-            remap += o+"="+absCWD+"/"+dirname+"/out/"+o+";"
-            #print("remap is "+ remap)
-	        #print("outname is "+outname)
-        #remove last semicolon 
-        remap = remap[:-1]
-        subf.write("transfer_output_remaps = \""+remap+"\"\n")	
+        remap= absCWD+"/"+dirname+"/out/"+outname
+        #print("remap is "+ remap)
+	#print("outname is "+outname)
+        subf.write("transfer_output_remaps = \""+outname+"="+remap+"\"\n")	
 
 #splits by event number
-def eventsSplit(infile, nChunk, filelist):
-    if filelist:
-        #should split by event number in file
-        tree = ROOT.TChain("tree/llpgtree")
-        with open(infile,"r") as f:
-            lines = f.readlines()
-            for line in lines:
-                rfile = ROOT.TFile.Open(line[:-1]) #remove new line character
-                tree.AddFile(rfile.GetName())
-    else:
-        #should split by event number in file
-        rfile = ROOT.TFile.Open(infile)
-        tree = rfile.Get("tree/llpgtree")
-    nevts = tree.GetEntries()
-    evts = range(nevts+1)
-    
+def eventsSplit(infile, nChunk):
     if nChunk == 0:
         nChunk += 1
+    #should split by event number in file
+    rfile = ROOT.TFile.Open(infile)
+    tree = rfile.Get("tree/llpgtree")
+    nevts = tree.GetEntries()
+    if nChunk > nevts:
+            print("Please pass split of at least",nevts)
+            return
     print("Splitting each file into "+str(nChunk)+" jobs ")
+    print("nevts",nevts)
+    evts = range(nevts+1)
     #return array of pairs of evtFirst and evtLast to pass as args into the exe to run
     #make sure to count first event in every chunk after first
     arr = [[min(i)-1, max(i)] for i in np.array_split(evts,nChunk)]
@@ -104,19 +82,17 @@ def writeQueueList( subf, inFile, ofilename, evts, flags ):
     #.root is set in exe
     outFileArg = ofilename+".$(Process)"
     
-    #infile should only be file name (no path)
-    match_str = "BayesianClustering/"
-    if match_str in inFile:
-        inFile = inFile[inFile.find(match_str)+len(match_str):]
-    #inFile = inFile[inFile.rfind("/")+1:]
-    #inFile = "root://cmsxrootd.fnal.gov/"+inFile
-    
     jobCtr=0
+    subf.write("\n\n\n")
+    subf.write("queue Arguments from (\n")
     for e in evts:
             inFileArg = " -i "+inFile
-            Args = "Arguments ="+inFileArg+" "+flags+" --evtFirst "+str(e[0])+" --evtLast "+str(e[1])+" -o "+outFileArg+"\n"
-            subf.write("\n\n\n")
+            #Args = "Arguments ="+inFileArg+" "+flags+" --evtFirst "+str(e[0])+" --evtLast "+str(e[1])+" -o "+outFileArg+"\n"
+            Args = inFileArg+" "+flags+" --evtFirst "+str(e[0])+" --evtLast "+str(e[1])+" -o "+outFileArg+"\n"
+            #subf.write("\n\n\n")
             subf.write("###### job"+str(jobCtr)+ "######\n")
             subf.write(Args)
-            subf.write("Queue\n")
+            #subf.write("Queue\n")
             jobCtr=jobCtr+1
+
+    subf.write(")")
--- condorSimNtuples/submissionHelper.py
+++ condorSimNtuples/submissionHelper.py
@@ -37,6 +37,7 @@ def writeSubmissionBase(subf, dirname, ofilename):
         #subf.write("transfer_input_files = /uscms/home/z374f439/nobackup/whatever_you_want/sandbox-CMSSW_10_6_5-6403d6f.tar.bz2, configSim.tgz, \n")
         subf.write("transfer_input_files = /uscms/home/mlazarov/nobackup/sandboxes/sandbox-CMSSW_13_0_13.tar.bz2, configSim.tgz, \n")
         subf.write("should_transfer_files = YES\n")
+        #subf.write("request_memory = 4096\n")
         subf.write("when_to_transfer_output = ON_EXIT\n")
         outname = ofilename+".$(Process).root"
         subf.write("transfer_output_files = "+outname+"\n")
@@ -51,14 +52,18 @@ def writeSubmissionBase(subf, dirname, ofilename):
         subf.write("transfer_output_remaps = \""+outname+"="+remap+"\"\n")	
 
 #splits by event number
-def eventsSplit(nevts, nChunk):
+def eventsSplit(infile, nChunk):
     if nChunk == 0:
         nChunk += 1
-    print("Splitting each file into "+str(nChunk)+" jobs ")
     #should split by event number in file
-    #rfile = ROOT.TFile.Open(infile)
-    #tree = rfile.Get("tree/llpgtree")
-    #nevts = tree.GetEntries()
+    rfile = ROOT.TFile.Open(infile)
+    tree = rfile.Get("tree/llpgtree")
+    nevts = tree.GetEntries()
+    if nChunk > nevts:
+            print("Please pass split of at least",nevts)
+            return
+    print("Splitting each file into "+str(nChunk)+" jobs ")
+    print("nevts",nevts)
     evts = range(nevts+1)
     #return array of pairs of evtFirst and evtLast to pass as args into the exe to run
     #make sure to count first event in every chunk after first
@@ -70,7 +75,7 @@ def eventsSplit(nevts, nChunk):
 
 
 # Write each job to the condor submit file.
-def writeQueueList( subf, ofilename, evts, flags ):
+def writeQueueList( subf, inFile, ofilename, evts, flags ):
     if evts == 0 or evts is None:
         print("No events found")
         return
@@ -78,10 +83,16 @@ def writeQueueList( subf, ofilename, evts, flags ):
     outFileArg = ofilename+".$(Process)"
     
     jobCtr=0
+    subf.write("\n\n\n")
+    subf.write("queue Arguments from (\n")
     for e in evts:
-            Args = "Arguments ="+" "+flags+" --evtFirst "+str(e[0])+" --evtLast "+str(e[1])+" -o "+outFileArg+"\n"
-            subf.write("\n\n\n")
+            inFileArg = " -i "+inFile
+            #Args = "Arguments ="+inFileArg+" "+flags+" --evtFirst "+str(e[0])+" --evtLast "+str(e[1])+" -o "+outFileArg+"\n"
+            Args = inFileArg+" "+flags+" --evtFirst "+str(e[0])+" --evtLast "+str(e[1])+" -o "+outFileArg+"\n"
+            #subf.write("\n\n\n")
             subf.write("###### job"+str(jobCtr)+ "######\n")
             subf.write(Args)
-            subf.write("Queue\n")
+            #subf.write("Queue\n")
             jobCtr=jobCtr+1
+
+    subf.write(")")
