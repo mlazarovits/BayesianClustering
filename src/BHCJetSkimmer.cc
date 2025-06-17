555bb6cc251759d583d1dc064c11ed45968f8b84
Margaret Lazarovits <mlazarovits@gmail.com>
2025-06-17 13:43:53 -0500

    subclustering reco AK8 and AK15 jets now - have hists already for # subclusters

--- src/BHCJetSkimmer.cc
+++ src/BHCJetSkimmer.cc
@@ -14,6 +14,17 @@ void BHCJetSkimmer::Skim(){
 		cout << " NlnN (Delauney) with reco AK4 rechits" << endl;
 	else
 		cout << " undefined. Please use SetStrategy(i) with i == 0 (NlnN), 1 (N2)" << endl;
+
+	//event selection
+	cout << "Using event selection";
+	if(_sel == boostW)
+		cout << " boosted Ws (fully hadronic)" << endl;
+	else if(_sel == boostTop)
+		cout << " boosted tops (fully hadronic)" << endl;
+	else if(_sel == QCDdijets)
+		cout << " QCD dijets" << endl;
+	else
+		cout << " default (2+ gen partons that are not tops, including gluons)" << endl;
 	
 	TFile* ofile = new TFile(_oname.c_str(),"RECREATE");
 	//cout << "oname " << _oname << endl;	
@@ -57,58 +68,97 @@ void BHCJetSkimmer::Skim(){
 		//at least 1 gen jet
 		_base->GetEntry(i);
 		if(i % (SKIP) == 0) cout << "evt: " << i << " of " << _nEvts << endl;
-		//if(_base->Jet_genNJet < 1) continue;
-		int ngenpart = _base->genpart_ngenpart;
-		//at least 1 top quark with pt, E requirements
-		int ntop = 0;
-		for(int g = 0; g < ngenpart; g++){
-			if(fabs(_base->genpart_id->at(g)) != 6) continue;
-			//if(_base->genpart_energy->at(g) < _minTopE) continue;
-			if(_base->genpart_pt->at(g) < _minTopPt) continue;
-			if(i % SKIP == 0) cout << " has top with pt " << _base->genpart_pt->at(g) << " and energy " << _base->genpart_energy->at(g) << " and id " << _base->genpart_id->at(g) << " and pz " << _base->genpart_pz->at(g) << endl;
-			ntop++;
-		}	
-
-		if(ntop < 1){
-			if(i % SKIP == 0) cout << " has no tops that pass pt > " << _minTopPt << endl;
-			continue;
-		}
-		//at least 1 W quark with pt, E requirements
-		int nW = 0;
-		for(int g = 0; g < ngenpart; g++){
-			if(fabs(_base->genpart_id->at(g)) != 24) continue;
-			if(_base->genpart_pt->at(g) < _minWPt) continue;
-			if(i % SKIP == 0) cout << " has W with pt " << _base->genpart_pt->at(g) << " and energy " << _base->genpart_energy->at(g) << " and id " << _base->genpart_id->at(g) << " and pz " << _base->genpart_pz->at(g) << endl;
-			nW++;
-		}	
-		//at least 1 W	
-		if(nW < 1){ 
-			if(i % SKIP == 0) cout << " has no Ws" << endl;
-			continue;
-		}
-
-		////at least 1 b	
-		//int nb = count(_base->genpart_id->begin(), _base->genpart_id->end(), 5);
-		//nb += count(_base->genpart_id->begin(), _base->genpart_id->end(), -5);
-		//if(nb < 1) continue;
 	
-		//reject fully leptonic and semi-leptonic W decays - later may want to turn off to look at just displaced b decays
-		//only look at hadronic W decays
-		int nW_lep = 0;	
-		for(int g = 0; g < ngenpart; g++){
-			int genmomidx = _base->genpart_momIdx->at(g);
-			if(genmomidx == -1) continue;
-			//skip particles that didn't come from a W
-			if(fabs(_base->genpart_id->at(genmomidx)) != 24) continue;
-			int genid = fabs(_base->genpart_id->at(g));
-			//only check for !neutrinos
-			if(genid == 11 || genid == 13 || genid == 15) nW_lep++; 
+		int ngenpart = _base->genpart_ngenpart;
+
+		if(_sel == boostW || _sel == boostTop){
+			//reject fully leptonic and semi-leptonic W decays for ttbar - later may want to turn off to look at just displaced b decays
+			//only look at hadronic W decays
+			int nW_lep = 0;	
+			for(int g = 0; g < ngenpart; g++){
+				int genmomidx = _base->genpart_momIdx->at(g);
+				if(genmomidx == -1) continue;
+				//skip particles that didn't come from a W
+				if(fabs(_base->genpart_id->at(genmomidx)) != 24) continue;
+				int genid = fabs(_base->genpart_id->at(g));
+				//only check for !neutrinos
+				if(genid == 11 || genid == 13 || genid == 15) nW_lep++; 
+			}
+			if(i % SKIP == 0) cout << " has " << nW_lep << " leptonic Ws" << endl;
+			//if any Ws in event decay leptonically, don't count
+			if(nW_lep > 0) continue;
+			//if all Ws in event decay leptonically, don't count
+			//if(nW - nW_lep == 0) continue;
 		}
-		if(i % SKIP == 0) cout << " has " << nW_lep << " leptonic Ws" << endl;
-		//if any Ws in event decay leptonically, don't count
-		if(nW_lep > 0) continue;
-		//if all Ws in event decay leptonically, don't count
-		//if(nW - nW_lep == 0) continue;
+
+		int nW = 0;
+		//do event selection here based on enum
+		if(_sel == boostW){
+			int totW = 0;
+			//at least 1 W quark with pt, E requirements
+			for(int g = 0; g < ngenpart; g++){
+				if(fabs(_base->genpart_id->at(g)) != 24) continue;
+				totW++;
+				if(_base->genpart_pt->at(g) < _minWPt) continue;
+				if(i % SKIP == 0) cout << " has W with pt " << _base->genpart_pt->at(g) << " and energy " << _base->genpart_energy->at(g) << " and id " << _base->genpart_id->at(g) << " and pz " << _base->genpart_pz->at(g) << endl;
+				nW++;
+			}	
+			//at least 1 W	
+			if(nW < 1){ 
+				if(i % SKIP == 0) cout << " has no Ws" << endl;
+				continue;
+			}
+			//check # b's - should match # Ws
+			int nb = count(_base->genpart_id->begin(), _base->genpart_id->end(), 5);
+			nb += count(_base->genpart_id->begin(), _base->genpart_id->end(), -5);
+			if(i % SKIP == 0) cout << " has " << nb << " gen b's and " << totW << " total gen W's" << endl; 
+			if(nb != nW) continue;
+
+
+		}
+		else if(_sel == boostTop){
+			//at least 1 top quark with pt, E requirements
+			int ntop = 0;
+			for(int g = 0; g < ngenpart; g++){
+				if(fabs(_base->genpart_id->at(g)) != 6) continue;
+				//if(_base->genpart_energy->at(g) < _minTopE) continue;
+				if(_base->genpart_pt->at(g) < _minTopPt) continue;
+				if(i % SKIP == 0) cout << " has top with pt " << _base->genpart_pt->at(g) << " and energy " << _base->genpart_energy->at(g) << " and id " << _base->genpart_id->at(g) << " and pz " << _base->genpart_pz->at(g) << endl;
+				ntop++;
+			}	
+
+			if(ntop < 1){
+				if(i % SKIP == 0) cout << " has no tops that pass pt > " << _minTopPt << endl;
+				continue;
+			}
+
+
+		}
+		else if(_sel == QCDdijets){
+			//at least two gen partons to be reconstructed as jets in event (ie saved gen partons)
+			int nparton = 0;
+			vector<int> p_ids = {1,2,3,4,5,21};
+			for(int g = 0; g < ngenpart; g++){
+				if(find(p_ids.begin(), p_ids.end(), fabs(_base->genpart_id->at(g))) == p_ids.end()) continue;
+				nparton++;
+			}	
+			if(nparton < 2) continue;
+
+		}
+		//default selection
+		else{
+			//at least two gen partons to be reconstructed as jets in event (ie saved gen partons)
+			int nparton = 0;
+			vector<int> p_ids = {1,2,3,4,5};
+			for(int g = 0; g < ngenpart; g++){
+				if(find(p_ids.begin(), p_ids.end(), fabs(_base->genpart_id->at(g))) == p_ids.end()) continue;
+				nparton++;
+			}	
+			if(nparton < 2) continue;
+
+		}
+	
+
 
 		_prod->GetGenJets(_genAK4jets, _genAK8jets, _genAK15jets, i);
 		//if(_genjets.size() < 1){ cout << endl; continue; }
@@ -134,7 +184,7 @@ void BHCJetSkimmer::Skim(){
 			if(rhs.size() < 1) continue;
 			x_nrhs_subcl.push_back((double)rhs.size());
 			
-			cout << "SubClustering reco jet #" << j << " with " << rhs.size() << " rec hits..." << endl;	
+			cout << "SubClustering reco AK4 jet #" << j << " with " << rhs.size() << " rec hits..." << endl;	
 			algo = new BayesCluster(rhs);
 			algo->SetMeasErrParams(_cell, _tresCte, _tresStoch*_gev, _tresNoise*_gev); 
 			if(_smear) algo->SetDataSmear(smear);
@@ -177,6 +227,66 @@ void BHCJetSkimmer::Skim(){
 			_procCats[p].hists1D[0][141]->Fill(nsubcls_tot);
 			_procCats[p].hists2D[0][39]->Fill(nsubcls_tot, (int)_recoAK4jets.size());
 		}
+		//do GMM for AK8
+		for(int j = 0; j < _recoAK8jets.size(); j++){
+			 _recoAK8jets[j].GetJets(rhs);
+			//safety
+			if(rhs.size() < 1) continue;
+			x_nrhs_subcl.push_back((double)rhs.size());
+			
+			cout << "SubClustering reco AK8 jet #" << j << " with " << rhs.size() << " rec hits..." << endl;	
+			algo = new BayesCluster(rhs);
+			algo->SetMeasErrParams(_cell, _tresCte, _tresStoch*_gev, _tresNoise*_gev); 
+			if(_smear) algo->SetDataSmear(smear);
+			algo->SetThresh(_thresh);
+			algo->SetAlpha(_alpha);
+			algo->SetSubclusterAlpha(_emAlpha);
+			algo->SetVerbosity(_verb);
+			algo->SetPriorParameters(_prior_params);
+			t = clock();
+			GaussianMixture* gmm = algo->SubCluster();
+			t = clock() - t;
+			y_time_subcl.push_back((double)t/CLOCKS_PER_SEC);
+			//cout <<  "y time_subcl entry " << y_time_subcl[y_time_subcl.size()-1] << " " << (double)t/CLOCKS_PER_SEC << endl;	
+			comptime_subcl->Fill((double)t/CLOCKS_PER_SEC);
+			
+			_recoAK8jets[j].SetModel(gmm, _gev);
+			
+			cout << " jet has " << gmm->GetNClusters() << " subclusters" << endl;// with parameters" << endl;
+			rhs.clear();
+		}
+
+		//do GMM for AK15
+		for(int j = 0; j < _recoAK15jets.size(); j++){
+			 _recoAK15jets[j].GetJets(rhs);
+			//safety
+			if(rhs.size() < 1) continue;
+			x_nrhs_subcl.push_back((double)rhs.size());
+			
+			cout << "SubClustering reco AK15 jet #" << j << " with " << rhs.size() << " rec hits..." << endl;	
+			algo = new BayesCluster(rhs);
+			algo->SetMeasErrParams(_cell, _tresCte, _tresStoch*_gev, _tresNoise*_gev); 
+			if(_smear) algo->SetDataSmear(smear);
+			algo->SetThresh(_thresh);
+			algo->SetAlpha(_alpha);
+			algo->SetSubclusterAlpha(_emAlpha);
+			algo->SetVerbosity(_verb);
+			algo->SetPriorParameters(_prior_params);
+			t = clock();
+			GaussianMixture* gmm = algo->SubCluster();
+			t = clock() - t;
+			y_time_subcl.push_back((double)t/CLOCKS_PER_SEC);
+			//cout <<  "y time_subcl entry " << y_time_subcl[y_time_subcl.size()-1] << " " << (double)t/CLOCKS_PER_SEC << endl;	
+			comptime_subcl->Fill((double)t/CLOCKS_PER_SEC);
+			
+			_recoAK15jets[j].SetModel(gmm, _gev);
+			
+			cout << " jet has " << gmm->GetNClusters() << " subclusters" << endl;// with parameters" << endl;
+			rhs.clear();
+		}
+
+
+
 		FillRecoJetHists();
 		//only does above
 		if(_strategy == gmmOnly){
@@ -242,9 +352,86 @@ void BHCJetSkimmer::Skim(){
 		y_time.push_back((double)t/CLOCKS_PER_SEC);
 		//cout <<  "y time entry " << y_time[y_time.size()-1] << " " << (double)t/CLOCKS_PER_SEC << endl;	
 		comptime->Fill((double)t/CLOCKS_PER_SEC);
+
 		cout << _trees.size() << " trees" << endl;
 		//transform trees (nodes) to jets
 		TreesToJets();
+		
+		//do jet-based evt selection for BHC jets
+		if(_sel == boostW){
+			//need at least nW*2 jets (ie 1 nW = 1 W jet and 1 b jet)
+			if(_predJets.size() != nW*2) continue;
+			//match subleading jets to gen b's using dR and Eratio - jets are sorted in TreesToJets
+			int nsub = nW; //if 1 W, expect 1 b; 2 Ws = 2 b;s
+			vector<Jet> subjets, leadjets;
+			if(nsub == 1){
+				subjets.push_back(_predJets[1]);
+				leadjets.push_back(_predJets[0]);
+			}
+			else{
+				leadjets.push_back(_predJets[0]);
+				leadjets.push_back(_predJets[1]);
+				subjets.push_back(_predJets[2]);
+				subjets.push_back(_predJets[3]);
+			}
+			vector<int> genbMatchIdxs(subjets.size(),-1);
+			GenericMatchJet(subjets,_genparts, genbMatchIdxs, 5); //dr match BHC jets to gen Ws
+			//want subleading jets to be very good matches to their respective gen b quarks (these are the "tags")
+			//good match = small dr, Eratio close to 1
+			bool drGood = true;
+			bool EratioGood = true;
+			vector<bool> bMatch(subjets.size(), 0);
+			for(int j = 0; j < subjets.size(); j++){
+				int genbidx = genbMatchIdxs[j];
+				//check dr
+				double dr = dR(subjets[j].eta(), subjets[j].phi(), _genparts[genbidx].eta(), _genparts[genbidx].phi());
+				//check Eratio
+				double Eratio = subjets[j].E()/_genparts[genbidx].E();
+				if(dr > 0.1) drGood = false;
+				if(Eratio < 0.8 || Eratio > 1.2) EratioGood = false;
+				bMatch[j] = drGood && EratioGood;
+			}
+			//could also add requirement that top mass is made with best W+b jet combinations
+			//if ALL good b-jet matches, remove from "predJets" list s.t. predJets are only W candidate jets
+			if(std::all_of(bMatch.begin(), bMatch.end(), [](bool v) { return v;})){
+				_predJets = leadjets;
+			}
+			//then gen-match remaining jet(s) to Ws (ie the "probes") - done in FillPredJets
+			else continue; //else, skip this event
+		
+		}
+		if(_sel == boostTop){
+			if(_predJets.size() < 2) continue; //want at least two boosted tops in an event
+			//if there are any jets that are good matches to bs then the top can't be boosted, skip
+			vector<int> genbMatchIdxs(_predJets.size(),-1);
+			GenericMatchJet(_predJets,_genparts, genbMatchIdxs, 5); //dr match BHC jets to gen Ws
+			//want subleading jets to be very good matches to their respective gen b quarks (these are the "tags")
+			//good match = small dr, Eratio close to 1
+			bool drGood = true;
+			bool EratioGood = true;
+			vector<bool> bMatch(_predJets.size(), 0);
+			for(int j = 0; j < _predJets.size(); j++){
+				int genbidx = genbMatchIdxs[j];
+				//check dr
+				double dr = dR(_predJets[j].eta(), _predJets[j].phi(), _genparts[genbidx].eta(), _genparts[genbidx].phi());
+				//check Eratio
+				double Eratio = _predJets[j].E()/_genparts[genbidx].E();
+				if(dr > 0.1) drGood = false;
+				if(Eratio < 0.8 || Eratio > 1.2) EratioGood = false;
+				bMatch[j] = drGood && EratioGood;
+			}
+			//if ANY good matches, skip	
+			if(std::any_of(bMatch.begin(), bMatch.end(), [](bool v) { return v;})){
+				continue;
+			}
+
+		}
+
+
+
+
+
+
 		//fill pred jet hists with jets
 		FillPredJetHists();
 		nsubcls_tot = 0;
