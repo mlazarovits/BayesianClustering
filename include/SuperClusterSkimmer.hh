36357c9d372316702da6d0469a31644b7014afa5
Margaret Lazarovits <mlazarovits@gmail.com>
2025-07-14 11:18:52 -0500

    removed phi center req from spike CR

--- include/SuperClusterSkimmer.hh
+++ include/SuperClusterSkimmer.hh
@@ -3754,7 +3754,8 @@ class SuperClusterSkimmer : public BaseSkimmer{
 			TrackMatched(gmm,ncl,bestdr, bestp);
 			//early times, phi left/right for BH
 			//if subcl is spike
-			if(bestdr < 0.02 && tc <= -8 && !(pc < 0.3) && !(acos(-1) - 0.3 < pc && pc < acos(-1) + 0.3) && !(2*acos(-1) - 0.3 < pc )){
+			//if(bestdr < 0.02 && tc <= -8 && !(pc < 0.3) && !(acos(-1) - 0.3 < pc && pc < acos(-1) + 0.3) && !(2*acos(-1) - 0.3 < pc )){
+			if(bestdr < 0.02 && tc <= -8){
 				label = 3;
 			}
 			else{
