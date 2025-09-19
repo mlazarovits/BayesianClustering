c77d7632cb33fb330d2ce7f53dbf1f56b165a88b
Margaret Lazarovits <mlazarovits@gmail.com>
2025-09-19 10:25:32 -0500

    rearranged couts

--- src/BaseProducer.cc
+++ src/BaseProducer.cc
@@ -157,7 +157,6 @@ void BaseProducer::GetTruePhotons(vector<Jet>& phos, int evt, double gev){
 	double timecorr, calibfactor; 
 	double drh, dpv;
 	int scidx;
-
 	vector<unsigned int> rhids = *_base->ECALRecHit_ID;
 	vector<unsigned int>::iterator rhit;
 
@@ -168,7 +167,7 @@ void BaseProducer::GetTruePhotons(vector<Jet>& phos, int evt, double gev){
 	vtx.SetValue(_base->PV_x, 0);
 	vtx.SetValue(_base->PV_y, 1);
 	vtx.SetValue(_base->PV_z, 2);
-cout << "n total phos " << nPhos << endl;
+cout << "\n evt " << evt << " n total phos " << nPhos << endl;
 	for(int p = 0; p < nPhos; p++){
 		//if selected photons # is already 2, return (only want 2 highest pt photons that pass selection)
 		if(selPhoCount == 2) return;
@@ -186,7 +185,6 @@ cout << "n total phos " << nPhos << endl;
 		scidx = _base->Photon_scIndex->at(p);
 
                 nrhs = _base->SuperCluster_rhIds->at(scidx).size();
-cout << "pt " << _base->Photon_pt->at(p) << " eta " << _base->Photon_eta->at(p) << endl;	
 		//hem veto?
 		if(_year == 2018 && _data){
 			hemVeto = ( (_base->Evt_run >= 319077) && (eta > -1.58) && (eta < -1.34) && (phi > 4.8) && (phi < 5.4) );
@@ -288,7 +286,7 @@ cout << "pt " << _base->Photon_pt->at(p) << " eta " << _base->Photon_eta->at(p) 
 			//else cout << "rechit with ID " << rhid << " not found in ECAL collection" << endl;
 
                 }
-		cout << "# rechits " << pho.GetNRecHits() << endl;	
+		cout << "pt " << _base->Photon_pt->at(p) << " eta " << _base->Photon_eta->at(p) << " # rechits " << pho.GetNRecHits() << endl;	
 		if(pho.GetNRecHits() < 2) continue;
 		selPhoCount++;
 	//	cout << jrhids.size() << " nrhs in pho" << endl;
