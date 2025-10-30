1baf390f9a0e56df60156247b9aa29e829046d3e
mlazarovits <mrlazarovits@gmail.com>
2025-10-30 14:14:26 -0500

    looking at logs and csvs (if exist)

--- condor/failedResubmissionTool.py
+++ condor/failedResubmissionTool.py
@@ -27,19 +27,34 @@ for l in f:
     subs.append(sub)
 print(len(subs),"subs")
 logs = glob.glob(BF_dir+"/log/*.out")
+#logs = glob.glob(BF_dir+"/out/*.csv")
 for i,log in enumerate(logs):
     jN = log.split("/")[-1]
     jN = jN[jN.find(".")+1:jN.rfind(".")]
     logs[i] = jN
 
 print(len(logs),"logs")
+csvs = glob.glob(BF_dir+"/out/*.csv")
+for i,csv in enumerate(csvs):
+    jN = csv.split("/")[-1]
+    jN = jN[jN.find(".")+1:jN.rfind(".")]
+    csvs[i] = jN
+
+print(len(csvs),"csvs")
+
 subSet = set(subs)
 logSet = set(logs)
+csvSet = set(csvs)
 
 subDiff = subSet.difference(logSet)
 print("Analyzing:", BF_dir)
-print("found", len(subDiff), "missing jobs")
+print("found", len(subDiff), "missing logs")
 jobs = ""
 for i in subDiff:
 	jobs += i+" "
+if(len(csvs) > 0):
+    subDiff = subSet.difference(csvSet)
+    print("found", len(subDiff), "missing csvs")
+    for i in subDiff:
+    	jobs += i+" "
 print(jobs)
