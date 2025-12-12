45c6cc73d016040420bc161357ada983a0e01c12
mlazarovits <mrlazarovits@gmail.com>
2025-12-12 13:45:50 -0600

    updated to run over signal grids

--- condor/generateSubmission.py
+++ condor/generateSubmission.py
@@ -17,6 +17,7 @@ def generateSubmission(args):
     m0 = [0, 0, 0]
     W0diag = [0.33333333,0.33333333,33.333333e-5]
     nu0 = 3
+    EMalpha = 1e-5
     # Ensure that the directory includes "/" at the end.
     odir = args.directory
     if odir[-1] != "/":
@@ -42,152 +43,160 @@ def generateSubmission(args):
     reco_date["2017_MC"] = "RunIIFall17DRPremix"
     reco_date["2018"] = ""
     inputFilterList = ""
-    if "GJets" in args.inputSample:
-        inputFileList = "kucmsntuple_GJets_R"+yr+"_"+sel+"_"+ver+"_GJets_HT-"+args.HT+"_TuneCP5_AODSIM_"+reco_date[args.year+"_MC"]+"RECO_list.txt"
-    elif "MET" in args.inputSample:
-        inputFileList = "kucmsntuple_MET_R"+yr+"_"+sel+"_"+ver+"_MET_AOD_Run20"+yr+args.era+reco_date[args.year+"_MET"]+"_list.txt"
-    elif "JetHT" in args.inputSample:
-    	inputFileList = "kucmsntuple_JetHT_R"+yr+"_"+sel+"_"+ver+"_JetHT_AOD_Run20"+yr+args.era+reco_date[args.year+"_MET"]+"_list.txt"
-    elif "QCD" in args.inputSample:
-    	inputFileList = "kucmsntuple_QCD_R"+yr+"_"+sel+"_"+ver+"_QCD_HT"+args.HT+"_TuneCP5_AODSIM_"+reco_date[args.year+"_MC"]+"RECO_list.txt"
-    elif "EGamma" in args.inputSample:
-        if "AL1SelEle" in sel:
-            inputFileList = "kucmsntuple_EGamma_R"+yr+"_"+sel+"_"+ver+"_EGamma_AOD_Run20"+yr+args.era+reco_date[args.year+"_DEG"]+"_UL2018-v1_list.txt"
-        else:
-            inputFileList = "kucmsntuple_EGamma_R"+yr+"_"+sel+"_"+ver+"_EGamma_AOD_Run20"+yr+args.era+"_list.txt"
-    elif "DoubleEG" in args.inputSample:
-        inputFileList = "kucmsntuple_DoubleEG_R"+yr+"_"+sel+"_"+ver+"_DoubleEG_AOD_Run20"+yr+args.era+reco_date[args.year+"_DEG"]+"_list.txt"
-    elif "gogo" in args.inputSample:
-        inputFileList = "kucmsntuple_SMS_Sig_"+sel+"_"+ver+"_SMS-GlGl_AODSIM_mGl-"+args.mGl+"_mN2-"+args.mN2+"_mN1-"+args.mN1+"_list.txt"
-    elif "sqsq" in args.inputSample:
-        inputFileList = "kucmsntuple_SMS_Sig_"+sel+"_"+ver+"_SMS-SqSq_AODSIM_mGl-"+args.mGl+"_mN2-"+args.mN2+"_mN1-"+args.mN1+"_list.txt"
-    else:
-    	print("Sample "+args.inputSample+" not found")
-    	exit()
+
+    data = False
+    if args.era is not None:
+        data = True
+
     #to use xrootd path cannot be relative
     #find any ../ and remove it and the dir before it
     inputPathList = "/uscms/home/mlazarov/nobackup/CMSSW_13_0_13/src/BayesianClustering/filelists/"
-    #inputPathList = "filelists/"
-    inputFileList = inputPathList+inputFileList
-
-    objName = args.object
-    #strategyName = "GMMonly" #only option for CMS jets/photons
-    
-    printstring = "Skimming for "+objName+" in file "+inputFileList
-    #if(objName == "jets"):
-    #	printstring += " with strategy "+strategyName 
-    print(printstring)
-    
-    #organize output by sample, object (ie jets or photons), and strategy (for jets only - NlnN or N2)
-    #find .root and then find the / before that (if it exists) - everything in between is the file name
-    match = "kucmsntuple_"
-    sampleName = inputFileList[ inputFileList.find(match)+len(match) : inputFileList.find("_list") ]
-    sampleNameShort = sampleName[ : sampleName.find("_")  ]
-
-    dirname = odir+sampleNameShort+"/"+sampleName+"/"+objName
-    ofilename = args.inputSample+"_"+objName
-   
-    #prior parameters
-    priorname = ""
-    
-    #add npergev to output name
-    gevstr = str(args.gev)
-    gevstr = gevstr.replace(".","p")
-    priorname += "_NperGeV-"+gevstr
-    
-    #if(objName == "jets"):
-    #	dirname += "/"+strategyName
-    #	ofilename += "_"+strategyName
-    #strategy doesn't apply to photons (GMM only)
-    
-    if args.output is not None and objName not in args.output:
-    	ofilename = ofilename+"_"+args.output 
-    	dirname = dirname+"_"+args.output
-    #dirname += priorname
-    #ofilename += priorname
-    ofilename = "condor_"+ofilename
-    
-    print("Preparing sample directory: {0}".format(dirname))
-    ##### Create a workspace (remove existing directory) #####
-    if os.path.exists(dirname):
-    	print("Removing existing directory: {0}".format(dirname))
-    	shutil.rmtree(dirname)
-    # Create directories for work area.
-    SH.createWorkArea(dirname)
-    
-    if(args.gev == -999):
-        if(args.object == "jets"):
-            args.gev = 1./10.
+    #loop through all files in filelist dir
+    condor_subs = []
+    for filelist in os.listdir(inputPathList):
+        if args.inputSample not in filelist:
+            continue
+        if sel not in filelist:
+            continue
+        if ver not in filelist:
+            continue
+        if "SMS" in args.inputSample:
+            if args.mGl is not None and args.mGl not in filelist:
+                continue
+            if args.mN2 is not None and args.mGl not in filelist:
+                continue
+            if args.mN1 is not None and args.mGl not in filelist:
+                continue
         else:
-            args.gev = 1./30.
-    
+            if args.HT is not None and args.HT not in filelist and not data:
+                continue
+            if args.year is not None and args.year not in filelist:
+                continue
+        if data:
+            if args.era not in filelist:
+                continue
+        print("filelist",filelist)
 
-    # grab relevant flags
-    filearr = SH.filesSplit(inputFileList, args.maxnevts, args.maxnfiles)
-    flags = '--EMalpha '+str(args.EMalpha)+' -v '+str(args.verbosity)+' -t '+str(args.thresh)+" --gev "+str(args.gev)+' --minpt '+str(args.minpt)+' --minNrhs '+str(args.minnrhs)+' --minemE '+str(args.minemE)+' --minRhE '+str(args.minRhE)+' --BHFilter '+str(args.beamHaloFilter)+' --beta0 '+str(beta0)+' --m0 '
-    for m in m0:
-        flags += str(m)+' '
-    flags += '--W0diag '
-    for w in W0diag:
-        flags += str(w)+' '
-    flags += '--nu0 '+str(nu0)
+        inputFileList = inputPathList+filelist
 
-    if(args.smear):
-    	flags += ' --smear'
-    if(args.timeSmear and "AODSIM" in inputFileList):
-    	flags += ' --timeSmear'
-    if(args.timeSmear and "AODSIM" not in inputFileList):
-        print("---timeSmear only for MC")
-    if(args.applyFrac):
-	    flags += ' --applyFrac'
-    if(args.noCalibrate):
-    	flags += ' --noCalibrate'
-    if(args.rejectSpikes):
-        flags += ' --rejectSpikes'
-    if(args.noIso):
-        flags += ' --noIso'
-        flags += ' --maxmet_cr '+str(args.maxmet_cr)+' --minphopt_cr '+str(args.minphopt_cr)+' --minht_cr '+str(args.minht_cr)+' --minjetpt_cr '+str(args.minjetpt_cr)
-    if(args.maxRhE != -999):
-        flags += ' --maxRhE '+str(args.maxRhE)
-    if(args.noSpatCorr):
-        flags += ' --noSpatCorr'
-    if(args.cleanSubclusters and "AODSIM" not in inputFileList):
-        flags += ' --cleanSubclusters'
-    if(args.cleanSubclusters and "AODSIM" in inputFileList):
-        print("--cleanSubclusters only for data") 
-    if(args.cleanMist and "AODSIM" not in inputFileList):
-        flags += ' --cleanMist'
-    if(args.cleanMist and "AODSIM" in inputFileList):
-        print("--cleanMist only for data") 
-    if(args.noLumiMask):
-        flags += ' --noLumiMask'
-    
+        objName = args.object
+        
+        printstring = "Skimming for "+objName+" in file "+inputFileList
+        #if(objName == "jets"):
+        #	printstring += " with strategy "+strategyName 
+        print(printstring)
+        
+        #organize output by sample, object (ie jets or photons), and strategy (for jets only - NlnN or N2)
+        #find .root and then find the / before that (if it exists) - everything in between is the file name
+        match = "kucmsntuple_"
+        sampleName = inputFileList[ inputFileList.find(match)+len(match) : inputFileList.find("_list") ]
+        sampleNameShort = sampleName[ : sampleName.find("_")  ]
 
+        dirname = odir+sampleNameShort+"/"+sampleName+"/"+objName
+        ofilename = args.inputSample+"_"+objName
+        
+        if args.output is not None and objName not in args.output:
+        	ofilename = ofilename+"_"+args.output 
+        	dirname = dirname+"_"+args.output
+        ofilename = "condor_"+ofilename
+        
+        print("Preparing sample directory: {0}".format(dirname))
+        ##### Create a workspace (remove existing directory) #####
+        if os.path.exists(dirname):
+        	print("Removing existing directory: {0}".format(dirname))
+        	shutil.rmtree(dirname)
+        # Create directories for work area.
+        SH.createWorkArea(dirname)
+        
+        if(args.gev == -999):
+            if(args.object == "jets"):
+                args.gev = 1./10.
+            else:
+                args.gev = 1./30.
+        
 
-    if(objName == "jets"):
-    	flags += " --object 0"
-    elif(objName == "superclusters"):
-    	flags += " --object 1"
-    elif(objName == "photons"):
-    	flags += " --object 2"
-    else:
-        print("Object",objName,"not recognized")
+        # grab relevant flags
+        filearr = SH.filesSplit(inputFileList, args.maxnevts, args.maxnfiles)
+        flags = '--EMalpha '+str(EMalpha)+' -v '+str(args.verbosity)+' -t '+str(args.thresh)+" --gev "+str(args.gev)+' --minpt '+str(args.minpt)+' --minNrhs '+str(args.minnrhs)+' --minemE '+str(args.minemE)+' --minRhE '+str(args.minRhE)+' --BHFilter '+str(args.beamHaloFilter)+' --beta0 '+str(beta0)+' --m0 '
+        for m in m0:
+            flags += str(m)+' '
+        flags += '--W0diag '
+        for w in W0diag:
+            flags += str(w)+' '
+        flags += '--nu0 '+str(nu0)
+
+        if(args.smear):
+        	flags += ' --smear'
+        if(args.timeSmear and "AODSIM" in inputFileList):
+        	flags += ' --timeSmear'
+        if(args.timeSmear and "AODSIM" not in inputFileList):
+            print("---timeSmear only for MC")
+        if(args.applyFrac):
+	        flags += ' --applyFrac'
+        if(args.noCalibrate):
+        	flags += ' --noCalibrate'
+        if(args.rejectSpikes):
+            flags += ' --rejectSpikes'
+        if(args.noIso):
+            flags += ' --noIso'
+            flags += ' --maxmet_cr '+str(args.maxmet_cr)+' --minphopt_cr '+str(args.minphopt_cr)+' --minht_cr '+str(args.minht_cr)+' --minjetpt_cr '+str(args.minjetpt_cr)
+        if(args.maxRhE != -999):
+            flags += ' --maxRhE '+str(args.maxRhE)
+        if(args.noSpatCorr):
+            flags += ' --noSpatCorr'
+        if(args.cleanSubclusters and "AODSIM" not in inputFileList):
+            flags += ' --cleanSubclusters'
+        if(args.cleanSubclusters and "AODSIM" in inputFileList):
+            print("--cleanSubclusters only for data") 
+        if(args.cleanMist and "AODSIM" not in inputFileList):
+            flags += ' --cleanMist'
+        if(args.cleanMist and "AODSIM" in inputFileList):
+            print("--cleanMist only for data") 
+        if(args.noLumiMask):
+            flags += ' --noLumiMask'
+        
+
+        if(objName == "jets"):
+        	flags += " --object 0"
+        elif(objName == "superclusters"):
+        	flags += " --object 1"
+        elif(objName == "photons"):
+        	flags += " --object 2"
+        else:
+            print("Object",objName,"not recognized")
+            exit()
+
+        ##### Create condor submission script in src directory #####
+        condorSubmitFile = dirname + "/src/submit.sh"
+        subf = open(condorSubmitFile, "w")
+        print("outputfile name "+ofilename)
+        SH.writeSubmissionBase(subf, dirname, ofilename, args.max_mat, args.max_idle, args.request_memory)
+        #need to remove local lpc path for actual args
+        inputFileList = inputFileList[inputFileList.rfind("/",0,inputFileList.rfind("/"))+1:]
+        print("inputfilelist",inputFileList)
+        SH.writeQueueList(subf, ofilename, filearr, flags)
+        condor_subs.append(condorSubmitFile)
+        print()
+    if len(condor_subs) < 1:
+        print("No file lists found for input sample",args.inputSample,"and selection",sel)
         exit()
+    elif len(condor_subs) < 2:
+        print("------------------------------------------------------------")
+        print("Submission ready, to run use:")
+        print("condor_submit "+condor_subs[0]+"\n")
+    else:
+        #write bash script
+        mult_bash_name = odir+sampleNameShort+"/"
+        if "SMS" in args.inputSample:
+            mult_bash_name += args.inputSample+"_"
+        mult_bash_name += objName+"_"+args.output+"_MultiSub.sh"
+        mult_bash = open(mult_bash_name, "w")
+        SH.writeMultiSubScript(mult_bash, condor_subs)
+        print("------------------------------------------------------------")
+        print("Submission ready, to run use:")
+        print("source "+mult_bash_name+"\n")
 
-    ##### Create condor submission script in src directory #####
-    condorSubmitFile = dirname + "/src/submit.sh"
-    subf = open(condorSubmitFile, "w")
-    print("outputfile name "+ofilename)
-    SH.writeSubmissionBase(subf, dirname, ofilename, args.max_mat, args.max_idle, args.request_memory)
-    #need to remove local lpc path for actual args
-    inputFileList = inputFileList[inputFileList.rfind("/",0,inputFileList.rfind("/"))+1:]
-    print("inputfilelist",inputFileList)
-    #SH.writeQueueList(subf, inputFileList, ofilename, eventnums, flags)
-    SH.writeQueueList(subf, ofilename, filearr, flags)
-    
-    print("------------------------------------------------------------")
-    print("Submission ready, to run use:")
-    print("condor_submit "+condorSubmitFile)
+
 
 def main():
     # options
@@ -199,22 +208,21 @@ def main():
     parser.add_argument('--maxnevts',help="maximum number of events to run over",default=-999,type=int)
     parser.add_argument('--maxnfiles',help="maximum number of files total",default=-999,type=int)
     #Ntuple file to run over
-    parser.add_argument('-inputSample','-i',help='Ntuple sample to create skims from',required=True,choices=['JetHT','EGamma','DoubleEG','GJets','QCD','MET','gogo','sqsq'])
-    parser.add_argument('--mGl',help='gluino mass for signal',default='2000')
-    parser.add_argument('--mN2',help='neutralino2 mass for signal',default='1950')
-    parser.add_argument('--mN1',help='neutralino1 mass for signal',default='1')
-    parser.add_argument('--HT',help='ht bin',default='100to200')
-    parser.add_argument('--era',help='era (run) for PDs',default='RunF')
+    parser.add_argument('-inputSample','-i',help='Ntuple sample to create skims from',required=True,choices=['JetHT','EGamma','DoubleEG','GJets','QCD','MET','SMS-GlGl','SMS-SqSq'])
+    parser.add_argument('--mGl',help='gluino mass for signal',default=None)
+    parser.add_argument('--mN2',help='neutralino2 mass for signal',default=None)
+    parser.add_argument('--mN1',help='neutralino1 mass for signal',default=None)
+    parser.add_argument('--HT',help='ht bin',default=None)
+    parser.add_argument('--era',help='era (run) for PDs',default=None)
     parser.add_argument('--selection',help='ntuple preselection or gluino mass',required=True)#choices=['MET100','AL1IsoPho','AL1IsoPhoMET100','MRL_MET100','MRL_None'],required=True)
     parser.add_argument('--output','-o',help='output label')
-    parser.add_argument('--year',help='year of sample',default='2017',choices=['2016','2017','2018','2022'])
+    parser.add_argument('--year',help='year of sample',default=None,choices=['2016','2017','2018','2022'])
     #which object to analyze (jets or photons currently supported)
     parser.add_argument('--object',help='which object to skim',choices=["jets","superclusters","photons"],required=True)
     #parser.add_argument('--strategy','-st',help='if skimming jets, which strategy to use for BHC (NlnN = 0 default, N2 = 1, GMM only = 2)',default=0,type=int,choices=[2,1,0])
     parser.add_argument('--verbosity','-v',help="verbosity",default=-1)
     
     #add algorithm parameters - emAlpha, priors, verbosity, thresh
-    parser.add_argument('--EMalpha','-EMa',help="alpha for GMM (EM algo)",default=1e-5)
     
     parser.add_argument('--thresh','-t',help='threshold for GMM clusters',default=1.)
     parser.add_argument('--gev',help='energy transfer factor (default = 1/10 for jets, 1/30 for photons + superclusters',default=1/10)
